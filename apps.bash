3d6fc44711bae167ec316209127fd9495d34967c
Nick Papior Andersen <nickpapior@gmail.com>
2012-11-12 14:07:44 +0000

    Added meep

--- apps.bash
+++ apps.bash
@@ -1,4 +1,5 @@
 source applications/gnuplot.bash
 source applications/xmgrace.bash
 #source applications/mpb.bash # [gmp,libunistring,guile]
+#source applications/meep.bash # [gmp,libunistring,guile]
 source applications/wannier.bash
