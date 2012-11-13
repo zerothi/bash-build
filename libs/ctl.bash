bb7dd9ee9578de3ec10409dec86b13e9dedeb732
Nick Papior Andersen <nickpapior@gmail.com>
2012-11-13 08:46:56 +0000

    libctl is only installed on the hpc nodes

--- libs/ctl.bash
+++ libs/ctl.bash
@@ -1,16 +1,22 @@
-add_package http://ab-initio.mit.edu/libctl/libctl-3.2.1.tar.gz
 
-pack_set -s $MAKE_PARALLEL -s $IS_MODULE
-
-pack_set --install-query $(pack_get --install-prefix)/lib/libctl.a
-
+# We will only install this on the super computer
+tmp="$(hostname)"
+if [ "${tmp:0:2}" == "n-" ]; then
+    
+    add_package http://ab-initio.mit.edu/libctl/libctl-3.2.1.tar.gz
+    
+    pack_set -s $MAKE_PARALLEL -s $IS_MODULE
+    
+    pack_set --install-query $(pack_get --install-prefix)/lib/libctl.a
+    
 # Install commands that it should run
-pack_set --command "./configure" \
-    --command-flag "--prefix $(pack_get --install-prefix)"
-
+    pack_set --command "./configure" \
+	--command-flag "--prefix $(pack_get --install-prefix)"
+    
 # Make commands
-pack_set --command "make $(get_make_parallel)"
-pack_set --command "make" \
-    --command-flag "install"
-
-pack_install
+    pack_set --command "make $(get_make_parallel)"
+    pack_set --command "make" \
+	--command-flag "install"
+    
+    pack_install
+fi
