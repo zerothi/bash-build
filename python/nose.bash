26f2691231019ea784e7707245a186e6abbb98b6
Nick Papior Andersen <nickpapior@gmail.com>
2012-11-28 17:28:24 +0000

    Possible bugfix for nose not installing nose.tools

--- python/nose.bash
+++ python/nose.bash
@@ -5,6 +5,9 @@ pack_set --install-query $(pack_get --install-prefix $(get_parent))/bin/nosetest
 
 # Add requirments when creating the module
 pack_set --module-requirement $(get_parent)
+
+# Install commands that it should run
+pack_set --command "sed -i -e 's/^\(packages[^\]]*\)/\1,\'nose.tools\'/' setup.py"
     
 # Install commands that it should run
 pack_set --command "$(get_parent_exec) setup.py install"
