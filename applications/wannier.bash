a47bcacdda5b0feb0a85bb5c3628c86be08f103c
Nick Papior Andersen <nickpapior@gmail.com>
2012-12-02 23:46:26 +0100

    Made sure that the module name did not explicitly contain the compiler name and version (makes loading implicit for all servers)

--- applications/wannier.bash
+++ applications/wannier.bash
@@ -48,7 +48,7 @@ create_module \
     --module-path $(get_installation_path)/modules-npa-apps \
     -n "\"Nick Papior Andersen's script for loading $(pack_get --package): $(get_c)\"" \
     -v $(pack_get --version) \
-    -M $(pack_get --alias).$(pack_get --version).$(get_c) \
+    -M $(pack_get --alias).$(pack_get --version)/$(get_c) \
     -P "/directory/should/not/exist" $(list --prefix '-L ' $(get_default_modules)) \
     $(list --prefix '-L ' --loop-cmd 'pack_get --module-name' $(pack_get --module-requirement)) \
     -L $(pack_get --module-name) 
