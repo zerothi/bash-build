7a1a7d7466623b8f368fffd788aef31552e6d5e4
Nick Papior Andersen <nickpapior@gmail.com>
2013-01-03 09:47:11 +0000

    Fixed a bug in the bison install script.

--- libs/bison.bash
+++ libs/bison.bash
@@ -14,6 +14,3 @@ pack_set --command "./configure" \
 pack_set --command "make $(get_make_parallel)"
 pack_set --command "make" \
     --command-flag "install"
-
-tmp="$(which bison)"
-[ "${tmp:0:1}" != "/" ] && \
