90f77470d646ffaa035e05201ed9fbf68a5775fc
Nick Papior Andersen <nickpapior@gmail.com>
2013-06-06 13:45:31 +0000

    Added new installations of siseta utilities.

--- applications/siesta-dev.bash
+++ applications/siesta-dev.bash
@@ -140,6 +140,19 @@ pack_set --command "cd ../../Eig2DOS"
 pack_set --command "make"
 pack_set --command "cp Eig2DOS $(pack_get --install-prefix)/bin/"
 
+pack_set --command "cd ../WFS"
+pack_set --command "make"
+pack_set --command "cp info_wfsx $(pack_get --install-prefix)/bin/"
+pack_set --command "cp readwf $(pack_get --install-prefix)/bin/"
+pack_set --command "cp readwfx $(pack_get --install-prefix)/bin/"
+pack_set --command "cp wfs2wfsx $(pack_get --install-prefix)/bin/"
+pack_set --command "cp wfsx2wfs $(pack_get --install-prefix)/bin/"
+
+pack_set --command "cd ../HSX"
+pack_set --command "make"
+pack_set --command "cp hs2hsx $(pack_get --install-prefix)/bin/"
+pack_set --command "cp hsx2hs $(pack_get --install-prefix)/bin/"
+
 pack_install
 
 create_module \
