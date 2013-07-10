47a0943a59db17a24142091867f30ea1884ace8d
Nick Papior Andersen <nickpapior@gmail.com>
2013-07-10 10:04:57 +0000

    Fixed the vasp groups permissions for both surt and the HPC cluster

--- applications/vasp-common-end.bash
+++ applications/vasp-common-end.bash
@@ -40,6 +40,16 @@ pack_set --command "cp $vdw $(pack_get --install-prefix)/data/$vdw"
 # Add an ENV-flag for the kernel to be copied
 pack_set --module-opt "--set-ENV VASP_VDWKERNEL=$(pack_get --install-prefix)/data/$vdw"
 
+# Ensure that the group is correctly set
+tmp="$(pack_get --install-prefix)/bin"
+if $(is_host n-) ; then
+    pack_set --command "chmod o-rwx $tmp/vasp*"
+    pack_set --command "chgrp nanotech $tmp/vasp*"
+elif $(is_host surt a0 b0 c0 d0 g0 m0 n0 q0 p0 a1 b1 c1 d1 g1 m1 n1 q1 p1) ; then
+    pack_set --command "chmod o-rwx $tmp/vasp*"
+    pack_set --command "chgrp vasp $tmp/vasp*"
+fi
+
 pack_install
 
 create_module \
