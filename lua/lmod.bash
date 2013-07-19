aa46b328b549f6c165a9ae3e78d185178faf89df
Nick Papior Andersen <nickpapior@gmail.com>
2013-07-19 10:23:42 +0100

    Updated lmod to the newest version 5.1.0

--- lua/lmod.bash
+++ lua/lmod.bash
@@ -2,12 +2,12 @@
 #  libtoolize/libtool
 #  automake
 
-add_package http://downloads.sourceforge.net/project/lmod/Lmod-5.0.1.tar.bz2
+add_package http://downloads.sourceforge.net/project/lmod/Lmod-5.1.0.tar.bz2
 
 pack_set -s $IS_MODULE
 
 pack_set --alias lmod
-pack_set --prefix-and-module $(pack_get --alias)/$(pack_get --version)/$(get_c)
+pack_set --prefix-and-module $(pack_get --alias)/$(get_c)
 pack_set --module-requirement lua
 
 pack_set --install-query $(pack_get --install-prefix)/lmod
