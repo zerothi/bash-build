52112dd0f23a4dc01ea0c4cb9a7d5a9da9e10c62
Nick Papior Andersen <nickpapior@gmail.com>
2013-08-15 23:32:17 +0100

    Gpaw test for check

--- python/gpaw.bash
+++ python/gpaw.bash
@@ -22,31 +22,31 @@ pack_set --command "echo '#' > $file"
 
 if $(is_c intel) ; then
     pack_set --command "sed -i '1 a\
-compiler = \'$CC $CFLAGS $MKL_LIB -mkl=sequential\'\n\
-mpicompiler = \'$MPICC $CFLAGS $MKL_LIB\'\n\
-libraries = [\'mkl_scalapack_lp64\',\'mkl_blacs_openmpi_lp64\',\'mkl_lapack95_lp64\',\'mkl_blas95_lp64\']\n\
-extra_link_args = [\'$MKL_LIB\',\'-mkl=sequential\']\n\
-platform_id = \'Xeon\'' $file"
+compiler = \"$CC $CFLAGS $MKL_LIB -mkl=sequential\"\n\
+mpicompiler = \"$MPICC $CFLAGS $MKL_LIB\"\n\
+libraries = [\"mkl_scalapack_lp64\",\"mkl_blacs_openmpi_lp64\",\"mkl_lapack95_lp64\",\"mkl_blas95_lp64\"]\n\
+extra_link_args = [\"$MKL_LIB\",\"-mkl=sequential\"]\n\
+platform_id = \"Xeon\"' $file"
 
 elif $(is_c gnu) ; then
     pack_set --module-requirement scalapack
     pack_set --command "sed -i '1 a\
-compiler = \'$CC $CFLAGS \'\n\
-mpicompiler = \'$MPICC $CFLAGS \'\n\
-library_dirs += [\'$(pack_get --install-prefix atlas)/lib\']' $file"
+compiler = \"$CC $CFLAGS \"\n\
+mpicompiler = \"$MPICC $CFLAGS \"\n\
+library_dirs += [\"$(pack_get --install-prefix atlas)/lib\"]' $file"
 
     if [ $(pack_installed atlas) -eq 1 ] ; then
 	pack_set --module-requirement atlas
 	pack_set --command "sed -i '$ a\
-library_dirs += [\'$(pack_get --install-prefix blas)/lib\']\n\
-library_dirs += [\'$(pack_get --install-prefix lapack)/lib\']\n\
-libraries = [\'scalapack\',\'lapack\',\'blas\']' $file"
+library_dirs += [\"$(pack_get --install-prefix blas)/lib\"]\n\
+library_dirs += [\"$(pack_get --install-prefix lapack)/lib\"]\n\
+libraries = [\"scalapack\",\"lapack\",\"blas\"]' $file"
 
     else
 	pack_set --module-requirement lapack
 	pack_set --command "sed -i '$ a\
-library_dirs += [\'$(pack_get --install-prefix atlas)/lib\']\n\
-libraries = [\'scalapack\',\'lapack\',\'f77blas\',\'cblas\',\'atlas\']' $file"
+library_dirs += [\"$(pack_get --install-prefix atlas)/lib\"]\n\
+libraries = [\"scalapack\",\"lapack\",\"f77blas\",\"cblas\",\"atlas\"]' $file"
 
     fi
 fi
@@ -54,19 +54,19 @@ fi
 tmp="$(list --prefix ,\' --suffix /include\' --loop-cmd 'pack_get --install-prefix' $(pack_get --module-requirement))"
 
 pack_set --command "sed -i '$ a\
-include_dirs += [\'$(pack_get --install-prefix openmpi)/include\']\n\
-extra_compile_args = \'$CFLAGS -std=c99\'.split(\' \')\n\
+include_dirs += [\"$(pack_get --install-prefix openmpi)/include\"]\n\
+extra_compile_args = \"$CFLAGS -std=c99\".split(\" \")\n\
 scalapack = True\n\
 \n\
 if scalapack:\n\
-    define_macros += [(\'GPAW_NO_UNDERSCORE_CBLACS\', \'1\')]\n\
-    define_macros += [(\'GPAW_NO_UNDERSCORE_CSCALAPACK\', \'1\')]\n\
+    define_macros += [(\"GPAW_NO_UNDERSCORE_CBLACS\", \"1\")]\n\
+    define_macros += [(\"GPAW_NO_UNDERSCORE_CSCALAPACK\", \"1\")]\n\
 \n\
 hdf5 = True\n\
-library_dirs += [\'$(pack_get --install-prefix hdf5)/lib\']\n\
-libraries += [\'hdf5_hl\',\'hdf5\']\n\
-library_dirs += [\'$(pack_get --install-prefix zlib)/lib\']\n\
-libraries += [\'z\']\n\
+library_dirs += [\"$(pack_get --install-prefix hdf5)/lib\"]\n\
+libraries += [\"hdf5_hl\",\"hdf5\"]\n\
+library_dirs += [\"$(pack_get --install-prefix zlib)/lib\"]\n\
+libraries += [\"z\"]\n\
 \n\
 # Add all directories for inclusion\n\
 include_dirs += [${tmp:2}]' $file"
