ec76e90dc9db58e2610b3a2312f9495a5d728ff2
Nick Papior Andersen <nickpapior@gmail.com>
2013-10-21 17:19:30 +0200

    Added kwant and netcdf4 for python (kwant not fully complete)

---
+++ python/kwant.bash
@@ -0,0 +1,51 @@
+add_package http://downloads.kwant-project.org/kwant/kwant-1.0.0.tar.gz
+
+pack_set -s $IS_MODULE
+
+pack_set --install-query $(pack_get --install-prefix)/bin/kwant
+
+pack_set --module-requirement cython \
+    --module-requirement numpy \
+    --module-requirement mumps-serial
+
+file=setup.cfg
+pack_set --command "echo '#' > $file"
+
+# Check for Intel MKL or not
+tmp_flags="$(list --LDFLAGS --Wlrpath $(pack_get --module-requirement mumps-serial) mumps-serial)"
+tmp_compiler=""
+if $(is_c intel) ; then
+
+    pack_set --command "sed -i '1 a\
+[lapack]\n\
+libraries = mkl_intel_lp64 mkl_sequential mkl_core mkl_def\n\
+' $file"
+    
+elif $(is_c gnu) ; then
+    tmp_compiler=unix
+    if [ $(pack_installed atlas) -eq 1 ] ; then
+	pack_set --module-requirement atlas
+    else
+	pack_set --module-requirement lapack
+    fi
+
+    pack_set --command "sed -i '1 a\
+[lapack]\n\
+libraries = f77blas cblas atlas atlas_lapack\n\
+' $file"
+
+else
+    doerr kwant "Could not determine compiler..."
+fi
+
+
+pack_set --command "sed -i '1 a\
+[mumps]\n\
+libraries = zmumps mumps_common pord metis scotch\n\
+extra_link_args = $tmp_flags \n\
+' $file"
+
+pack_set --command "CFLAGS='$CFLAGS $tmp_flags' $(get_parent_exec) setup.py build"
+
+pack_set --command "$(get_parent_exec) setup.py install" \
+    --command-flag "--prefix=$(pack_get --install-prefix)"
---
+++ python/netcdf4.bash
@@ -0,0 +1,39 @@
+add_package --alias py-netcdf --package py-netcdf http://netcdf4-python.googlecode.com/files/netCDF4-1.0.6.tar.gz
+
+pack_set -s $IS_MODULE
+
+pack_set --install-query $(pack_get --install-prefix)/bin/nc3tonc4
+
+pack_set --module-requirement cython \
+    --module-requirement netcdf-serial \
+    --module-requirement hdf5-serial \
+    --module-requirement numpy
+
+# Check for Intel MKL or not
+tmp_flags="$(list --LDFLAGS --Wlrpath netcdf-serial hdf5-serial)"
+tmp_compiler=""
+if $(is_c intel) ; then
+    tmp_compiler="intelem"
+
+elif $(is_c gnu) ; then
+    tmp_compiler=unix
+
+else
+    doerr netCDF4 "Could not determine compiler..."
+fi
+
+file=setup.cfg
+pack_set --command "echo '#' > $file"
+
+pack_set --command "sed -i '1 a\
+[options]\n\
+use_ncconfig = True\n\
+[directories]\n\
+netCDF4_dir = $(pack_get --install-prefix netcdf-serial)\n\
+HDF5_dir = $(pack_get --install-prefix hdf5-serial)\n\
+' $file"
+
+pack_set --command "CFLAGS='$CFLAGS $tmp_flags' $(get_parent_exec) setup.py build"
+
+pack_set --command "$(get_parent_exec) setup.py install" \
+    --command-flag "--prefix=$(pack_get --install-prefix)"
