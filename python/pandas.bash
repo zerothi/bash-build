b254394bef81cd4372a113de1015dca1ad0d78af
Nick Papior Andersen <nickpapior@gmail.com>
2013-10-22 15:28:50 +0200

    Bugfix for panda and pytz libraryies

--- python/pandas.bash
+++ python/pandas.bash
@@ -1,4 +1,4 @@
-for v in 0.12.0 ; do 
+v=0.12.0
 add_package https://pypi.python.org/packages/source/p/pandas/pandas-$v.tar.gz
 
 pack_set -s $IS_MODULE -s $PRELOAD_MODULE
@@ -7,23 +7,10 @@ pack_set --install-query $(pack_get --install-prefix)/lib/python$pV/site-package
 
 pack_set $(list --prefix ' --module-requirement ' cython numpy numexpr[2] scipy pytables matplotlib bottleneck pytz)
 
+pack_set --command "mkdir -p $(pack_get --install-prefix)/lib/python$pV/site-packages"
+
 pack_set --command "$(get_parent_exec) setup.py build"
-#
-if [ 0 -eq 1 ]; then
-if $(is_c intel) ; then
-    pack_set --command "unset LDFLAGS && $(get_parent_exec) setup.py build" \
-	--command-flag "--compiler=intelem"
 
-elif $(is_c gnu) ; then
-    pack_set --command "unset LDFLAGS && $(get_parent_exec) setup.py build" \
-	--command-flag "--compiler=unix"
-
-else
-    doerr $(pack_get --package) "Could not recognize the compiler: $(get_c)"
-fi
-fi
-# Install commands that it should run
 pack_set --command "$(get_parent_exec) setup.py install" \
     --command-flag "--prefix=$(pack_get --install-prefix)"
 
-done
--- python/pytz.bash
+++ python/pytz.bash
@@ -4,5 +4,7 @@ pack_set -s $IS_MODULE -s $PRELOAD_MODULE
 
 pack_set --install-query $(pack_get --install-prefix)/lib/python$pV/site-packages/site.py
 
+pack_set --command "mkdir -p $(pack_get --install-prefix)/lib/python$pV/site-packages"
+
 pack_set --command "$(get_parent_exec) setup.py install" \
     --command-flag "--prefix=$(pack_get --install-prefix)"
