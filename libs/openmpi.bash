44e7530b578d3607ecc1098a3d20c50b75f0537c
Nick Papior Andersen <nickpapior@gmail.com>
2013-12-30 13:52:18 +0000

    Bugfix for the gnu-compiler for OpenMPI

--- libs/openmpi.bash
+++ libs/openmpi.bash
@@ -1,6 +1,6 @@
 # apt-get libc6-dev
 
-add_package http://www.open-mpi.org/software/ompi/v1.7/downloads/openmpi-1.6.5.tar.gz
+add_package http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz
 
 pack_set -s $BUILD_DIR -s $MAKE_PARALLEL -s $IS_MODULE
 
@@ -24,13 +24,24 @@ elif $(is_host thul) ; then
 
 fi
 
+if [ $(pack_get --installed flex) -eq 1 ]; then
+    pack_set --command "module load $(pack_get --module-name-requirement flex) $(pack_get --module-name flex)"
+fi
+
+
 # Install commands that it should run
 pack_set --command "../configure $tmp_flags" \
     --command-flag "--prefix=$(pack_get --install-prefix)" \
     --command-flag "--with-hwloc=$(pack_get --install-prefix hwloc)"
+
+# Fix for the GNU-compiler (it just removes erroneous library linkers)
+pack_set --command "sed -i -e '/postdeps/{s:-l ::gi}' libtool"
 
 # Make commands
 pack_set --command "make $(get_make_parallel)"
 pack_set --command "make" \
     --command-flag "install"
 
+if [ $(pack_get --installed flex) -eq 1 ] ; then
+    pack_set --command "module unload $(pack_get --module-name flex) $(pack_get --module-name-requirement flex)"
+fi
