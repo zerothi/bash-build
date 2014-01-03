4ce866f0490df9720fa3518581b492cc8f06fa1c
Nick Papior Andersen <nickpapior@gmail.com>
2014-01-03 12:30:16 +0000

    Added the compliation of the tmglib for lapack, which enables the compilation of the plasma-library with lapack

--- libs/lapack.bash
+++ libs/lapack.bash
@@ -5,27 +5,40 @@ pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 
 pack_set --install-query $(pack_get --install-prefix)/lib/liblapack.a
 
+pack_set --module-requirement blas
+
 # Prepare the make file
+file=make.inc
 tmp="sed -i -e"
-pack_set --command "cp make.inc.example make.inc"
-pack_set --command "$tmp 's/FORTRAN[[:space:]]*=.*/FORTRAN = $FC/g' make.inc"
-pack_set --command "$tmp 's/CC[[:space:]]*=.*/CC = $CC/g' make.inc"
-pack_set --command "$tmp 's/ARCH[[:space:]]*=.*/ARCH = $AR/g' make.inc"
-pack_set --command "$tmp 's/OPTS[[:space:]]*=.*/OPTS = $FCFLAGS/g' make.inc"
-pack_set --command "$tmp 's/CFLAGS[[:space:]]*=.*/CFLAGS = $CFLAGS/g' make.inc"
-pack_set --command "$tmp 's/NOOPT[[:space:]]*=.*/NOOPT = -fPIC/g' make.inc"
-pack_set --command "$tmp 's/LOADER[[:space:]]*=.*/LOADER = $FC/g' make.inc"
-pack_set --command "$tmp 's/LOADOPTS[[:space:]]*=.*/LOADOPTS = $FCFLAGS/g' make.inc"
-pack_set --command "$tmp 's/TIMER[[:space:]]*=.*/TIMER = INT_CPU_TIME/g' make.inc"
-pack_set --command "$tmp 's/_LINUX//g' make.inc"
-pack_set --command "$tmp 's/_SUN4//g' make.inc"
-pack_set --command "$tmp 's?BLASLIB[[:space:]]*=.*?BLASLIB = $(pack_get --install-prefix blas)/lib/libblas.a?g' make.inc"
+pack_set --command "cp $file.example $file"
+pack_set --command "$tmp 's/FORTRAN[[:space:]]*=.*/FORTRAN = $FC/g' $file"
+pack_set --command "$tmp 's/CC[[:space:]]*=.*/CC = $CC/g' $file"
+pack_set --command "$tmp 's/ARCH[[:space:]]*=.*/ARCH = $AR/g' $file"
+if $(is_c gnu) ; then
+    pack_set --command "$tmp 's/OPTS[[:space:]]*=.*/OPTS = $FCFLAGS -frecursive/g' $file"
+else
+    pack_set --command "$tmp 's/OPTS[[:space:]]*=.*/OPTS = $FCFLAGS/g' $file"
+fi 
+pack_set --command "$tmp 's/CFLAGS[[:space:]]*=.*/CFLAGS = $CFLAGS/g' $file"
+if $(is_c gnu) ; then
+    pack_set --command "$tmp 's/NOOPT[[:space:]]*=.*/NOOPT = -fPIC -frecursive/g' $file"
+else
+    pack_set --command "$tmp 's/NOOPT[[:space:]]*=.*/NOOPT = -fPIC/g' $file"
+fi
+pack_set --command "$tmp 's/LOADER[[:space:]]*=.*/LOADER = $FC/g' $file"
+pack_set --command "$tmp 's/LOADOPTS[[:space:]]*=.*/LOADOPTS = $FCFLAGS/g' $file"
+pack_set --command "$tmp 's/TIMER[[:space:]]*=.*/TIMER = INT_CPU_TIME/g' $file"
+pack_set --command "$tmp 's/_LINUX//g' $file"
+pack_set --command "$tmp 's/_SUN4//g' $file"
+pack_set --command "$tmp 's?BLASLIB[[:space:]]*=.*?BLASLIB = $(pack_get --install-prefix blas)/lib/libblas.a?g' $file"
+pack_set --command "echo '' >> $file"
+pack_set --command "echo 'LAPACKE_WITH_TMG = Yes' >> $file"
 
 # Make commands
-pack_set --command "make $(get_make_parallel) lapacklib lapackelib"
+pack_set --command "make $(get_make_parallel) lapacklib lapackelib tmglib"
 
 pack_set --command "mkdir -p $(pack_get --install-prefix)/lib/"
 pack_set --command "cp liblapack.a liblapacke.a $(pack_get --install-prefix)/lib/"
+pack_set --command "cp libtmglib.a $(pack_get --install-prefix)/lib/libtmg.a"
 pack_set --command "mkdir -p $(pack_get --install-prefix)/include/"
 pack_set --command "cp lapacke/include/*.h $(pack_get --install-prefix)/include/"
-
--- libs/plasma.bash
+++ libs/plasma.bash
@@ -1,7 +1,7 @@
 add_package \
     http://icl.cs.utk.edu/projectsfiles/plasma/pubs/plasma_2.6.0.tar.gz
 
-pack_set -s $IS_MODULE
+pack_set -s $IS_MODULE -s $MAKE_PARALLEL 
 
 pack_set --install-query $(pack_get --install-prefix)/lib/libplasma.a
 
@@ -26,14 +26,13 @@ if $(is_c gnu) ; then
     if [ $(pack_installed atlas) -eq 1 ]; then
 	pack_set --command "sed -i '1 a\
 CFLAGS  += -fopenmp \n\
-LIBBLAS  = $(list --LDFLAGS --Wlrpath atlas) -lf77blas -latlas \n\
-LIBCBLAS = $(list --LDFLAGS --Wlrpath atlas) -lcblas -latlas\n\
-LIBLAPACK = $(list --LDFLAGS --Wlrpath atlas) -llapack_atlas -latlas \n' $tmp"
+LIBBLAS  = $(list --LDFLAGS --Wlrpath atlas) -lf77blas -lcblas -latlas \n\
+LIBLAPACK = $(list --LDFLAGS --Wlrpath lapack atlas) -ltmg -llapack_atlas\n' $tmp"
     else
 	pack_set --command "sed -i '1 a\
 LIBBLAS  = $(list --LDFLAGS --Wlrpath blas) -lblas \n\
 LIBCBLAS = $(list --LDFLAGS --Wlrpath blas) -lcblas \n\
-LIBLAPACK = $(list --LDFLAGS --Wlrpath lapack) -llapack\n' $tmp"
+LIBLAPACK = $(list --LDFLAGS --Wlrpath lapack) -ltmg -llapack\n' $tmp"
     fi
 
 elif $(is_c intel) ; then
@@ -44,7 +43,7 @@ LDFLAGS += -nofor-main \n\
 LIBBLAS  = $MKL_LIB -lmkl_blas95_lp64 -mkl=parallel \n\
 LIBCBLAS  = $MKL_LIB -lmkl_blas95_lp64 -mkl=parallel \n\
 LIBLAPACK = $MKL_LIB -lmkl_lapack95_lp64 -mkl=parallel \n' $tmp"
-
+# The tmg-lib must be included...
 fi
 
 pack_set --command "sed -i '1 a\
@@ -54,13 +53,12 @@ LIBCLAPACK = $(list --LDFLAGS --Wlrpath lapack) -llapacke \n' $tmp"
 pack_set --command "sed -i '1 a\
 PLASMA_F90 =1\n\
 prefix = $(pack_get --install-prefix)\n\
-CC = $CC -DADD_ \n\
+CC = $CC \n\
 FC = $FC \n\
 LOADER = \$(FC) \n\
 ARCH = $AR \n\
 ARCHFLAGS = cr \n\
 RANLIB = ranlib \n\
-\n\
 CFLAGS = $CFLAGS -DADD_\n\
 FFLAGS = $FFLAGS \n\
 LDFLAGS = $FFLAGS \n' $tmp"
@@ -68,3 +66,4 @@ LDFLAGS = $FFLAGS \n' $tmp"
 # Make and install commands
 pack_set --command "make $(get_make_parallel) all"
 pack_set --command "make install"
+
