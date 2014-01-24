30730f0a6befa7f725441ade3c4f3acb9fd6a784
Nick Papior Andersen <nickpapior@gmail.com>
2014-01-24 12:07:14 +0100

    Added cffi pre-create directory

--- python/cffi.bash
+++ python/cffi.bash
@@ -9,6 +9,8 @@ pack_set --install-query $(pack_get --install-prefix)/lib/python$pV/site-package
 
 pack_set --module-requirement cython
 
+pack_set --command "mkdir -p $(pack_get --install-prefix)/lib/python$pV/site-packages"
+
 pack_set --command "$(get_parent_exec) setup.py install" \
     --command-flag "--prefix=$(pack_get --install-prefix)"
 
