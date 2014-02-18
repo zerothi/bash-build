dbca05d062240e3323412675c8740b8c2c1d8b9b
Nick Papior Andersen <nickpapior@gmail.com>
2014-02-18 22:45:22 +0000

    Added a build for PGI compilers

---
+++ build-examples/build-pgi.sh
@@ -0,0 +1,20 @@
+module purge
+
+source source-pgi.sh
+
+new_build --name pgi \
+    --installation-path /opt \
+    --module-path /opt/modules \
+    --build-path .compile \
+    --build-module-path "--package --version $(get_c)" \
+    --build-installation-path "--package --version $(get_c)" \
+    --source source-pgi.sh
+
+mkdir -p $(build_get --module-path[pgi])-npa
+mkdir -p $(build_get --module-path[pgi])-npa-apps
+
+build_set --default-module-version
+#FORCEMODULE=1
+#build_set --module-format LUA
+
+source build-generic.sh
---
+++ build-examples/source-pgi.sh
@@ -0,0 +1,41 @@
+set_c pgi-13.10-0
+
+AR=ar
+CC=pgcc
+CXX=pgcpp
+#CPP="gcc -E"
+#CXXCPP="g++ -E"
+F77=pgf77
+F90=pgf95
+FC=pgf95
+common_flags="-m64 -fPIC -O3 -Munroll -Mvect=prefetch -Mnofpapprox -Mnofprelaxed"
+CFLAGS="$common_flags"
+FCFLAGS="$common_flags -Mnosecond_underscore"
+FFLAGS="$common_flags -Mnosecond_underscore"
+
+MPICC=mpicc
+#MPICPP="mpicc -E"
+MPICXX=mpicxx
+#MPICXXCPP="mpicxx -E"
+MPIFC=mpif90
+MPIF77=mpif77
+MPIF90=mpif90
+
+LDFLAGS=
+
+export AR
+export CC
+export CXX
+export CPP
+export CXXCPP
+export FC
+export F77
+export F90
+export LDFLAGS
+export CFLAGS
+export FCFLAGS
+export FFLAGS
+export MPICC
+export MPIFC
+export MPIF77
+export MPIF90
