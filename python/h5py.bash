5aadd689f8bc98dc7f2aadd4a307be66d2aae76c
Nick Papior Andersen <nickpapior@gmail.com>
2014-05-03 10:59:56 +0000

    Currently we have troubles installing h5py with mpi support, it is the linking

--- python/h5py.bash
+++ python/h5py.bash
@@ -1,16 +1,17 @@
 v=2.3.0
 add_package https://pypi.python.org/packages/source/h/h5py/h5py-$v.tar.gz
 
-pack_set -s $IS_MODULE
+pack_set -s $IS_MODULE -s $PRELOAD_MODULE
 
 pack_set --install-query $(pack_get --install-prefix)/lib/python$pV/site-packages/$(pack_get --alias)
 
 # Add requirments when creating the module
 pack_set --module-requirement numpy \
-    --module-requirement zlib \
     --module-requirement hdf5-serial \
     --module-requirement cython
 
+# create dir
+pack_set --command "mkdir -p $(pack_get --install-prefix)/lib/python$pV/site-packages"
     
 # Install commands that it should run
 pack_set --command "$(get_parent_exec) setup.py build" \
