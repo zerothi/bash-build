5e09c8bb7e017da446fd1722feb761049d99d919
Nick Papior Andersen <nickpapior@gmail.com>
2014-07-29 14:01:43 +0200

    Reverted scalapack to rely on atlas. Prohibits the usage of mkl in certain situations

--- libs/scalapack.bash
+++ libs/scalapack.bash
@@ -18,30 +18,24 @@ pack_set --command "$tmp 's/FCFLAGS[[:space:]]*=.*/FCFLAGS = $FCFLAGS/g' $file"
 pack_set --command "$tmp 's/CCFLAGS[[:space:]]*=.*/CCFLAGS = $CFLAGS/g' $file"
 pack_set --command "$tmp 's/ARCH[[:space:]]*=.*/ARCH = $AR/g' $file"
 
+tmp_lib=
 if $(is_c intel) ; then
-    pack_set --command "$tmp 's|BLASLIB[[:space:]]*=.*|BLASLIB = $MKL_LIB -mkl=sequential -lmkl_blas95_lp64|g' $file"
-    pack_set --command "$tmp 's|^LAPACKLIB[[:space:]]*=.*|LAPACKLIB = -mkl=sequential -lmkl_lapack95_lp64|g' $file"
+    tmp_lib="-lgfortran"
+fi
 
-else
 if [ $(pack_installed atlas) -eq 1 ]; then
-    pack_set --command "module load" \
-	--command-flag "$(pack_get --module-requirement atlas)" \
-	--command-flag "$(pack_get --module-name atlas)"
-    pack_set --command "$tmp 's|BLASLIB[[:space:]]*=.*|BLASLIB = $(list --LDFLAGS --Wlrpath atlas) -lf77blas -lcblas -latlas|g' $file"
+    pack_set --command "module load $(pack_get --module-load atlas)"
+    pack_set --command "$tmp 's|BLASLIB[[:space:]]*=.*|BLASLIB = $tmp_lib $(list --LDFLAGS --Wlrpath atlas) -lf77blas -lcblas -latlas|g' $file"
     # No matter the source we need gfortran as ATLAS is compiled with GCC (we should really look into this)
     pack_set --command "$tmp 's|^LAPACKLIB[[:space:]]*=.*|LAPACKLIB = $(list --LDFLAGS --Wlrpath atlas) -llapack_atlas|g' $file"
 
 else
-    pack_set --command "module load" \
-	--command-flag "$(pack_get --module-requirement blas lapack)" \
-	--command-flag "$(pack_get --module-name blas lapack)"
+    pack_set --command "module load $(pack_get --module-load lapack)"
     pack_set --command "$tmp 's|BLASLIB[[:space:]]*=.*|BLASLIB = $(list --LDFLAGS --Wlrpath blas) -lblas|g' $file"
     
     pack_set --command "$tmp 's|^LAPACKLIB[[:space:]]*=.*|LAPACKLIB = $(list --LDFLAGS --Wlrpath lapack) -llapack|g' $file"
     
 fi
-fi
-
 
 # Make commands
 pack_set --command "make $(get_make_parallel)"
@@ -49,17 +43,9 @@ pack_set --command "make $(get_make_parallel)"
 pack_set --command "mkdir -p $(pack_get --install-prefix)/lib/"
 pack_set --command "cp libscalapack.a $(pack_get --install-prefix)/lib/"
 
-if $(is_c intel) ; then
-    echo "empty" > /dev/null
-else
 if [ $(pack_installed atlas) -eq 1 ]; then
-    pack_set --command "module unload" \
-	--command-flag "$(pack_get --module-name atlas)" \
-	--command-flag "$(pack_get --module-requirement atlas)"
+    pack_set --command "module unload $(pack_get --module-load atlas)"
 else
-    pack_set --command "module unload" \
-	--command-flag "$(pack_get --module-name lapack blas)" \
-	--command-flag "$(pack_get --module-requirement lapack blas)"
-fi
+    pack_set --command "module unload $(pack_get --module-load lapack)"
 fi
 
