df6f12d04d93da71e3960640ba18a93d5996ccc9
Nick Papior Andersen <nickpapior@gmail.com>
2014-09-19 15:13:38 +0200

    Fixed libffi

--- libs/libffi.bash
+++ libs/libffi.bash
@@ -21,21 +21,13 @@ pack_set --command "make check > tmp.test 2>&1"
 pack_set --command "make install"
 pack_set_mv_test tmp.test
 
-flib="$(pack_get --install-prefix)/lib/libffi-$(pack_get --version)/include"
-flib64="$(pack_get --install-prefix)/lib64/libffi-$(pack_get --version)/include"
-tinc="$(pack_get --install-prefix)/include"
-
-# Fix the include placement
-pack_set --command "[ -d $flib ] && mv $flib $tinc || true"
-pack_set --command "[ -d $flib64 ] && mv $flib $tinc || true"
-
-# Fix pkgconfig
+# Fix include path and pkgconfig
 for f in lib lib64 ; do
     flib="$(pack_get --install-prefix)/$f/pkgconfig/libffi.pc"
     pack_set --command "[ -e $flib ] && sed -i -e 's:includedir=.*:includedir=\${prefix}/include:' $flib || true"
     flib="$(pack_get --install-prefix)/$f/libffi-$(pack_get --version)"
-    pack_set --command "[ -d $flib ] && rm -rf $(pack_get --install-prefix)/lib/libffi-$(pack_get --version) || true"
+    pack_set --command "[ -d $flib/include ] && mv $flib/include $(pack_get --install-prefix)/include || true"
+    pack_set --command "[ -d $flib ] && rm -rf $flib || true"
 done
 unset flib
-unset flib64
 unset tinc
