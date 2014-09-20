bb0e465c6ff3d0976b35206340061f316064a62d
Nick Papior Andersen <nickpapior@gmail.com>
2014-09-20 14:43:33 +0000

    Updated hwloc and re-enabled numactl build-in

--- libs/hwloc.bash
+++ libs/hwloc.bash
@@ -1,24 +1,17 @@
-# apt-get install libxml2
-add_package http://www.open-mpi.org/software/hwloc/v1.9/downloads/hwloc-1.9.tar.bz2
+add_package http://www.open-mpi.org/software/hwloc/v1.9/downloads/hwloc-1.9.1.tar.bz2
 
 pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 
 pack_set --install-query $(pack_get --library-path)/libhwloc.a
 
-pack_set --module-requirement libxml2
-
-# We don't need the libnuma (it only provides an interface to 
-# use the libnuma routines for accessing hwloc)
-# Patch hwloc 1.9
-pack_set --command "wget http://www.student.dtu.dk/~nicpa/packages/patch_hwloc-intel"
-pack_set --command "patch -p1 < patch_hwloc-intel"
+pack_set --module-requirement numactl --module-requirement libxml2
 
 # Preload modules
 pack_set --command "module load help2man m4 autoconf automake"
 
 pack_set --command "./configure" \
     --command-flag "--prefix $(pack_get --install-prefix)" \
-    --command-flag "--disable-libnuma" \
+    --command-flag "--enable-libnuma" \
     --command-flag "--disable-opencl" \
     --command-flag "--disable-cuda" \
     --command-flag "--disable-nvml" \
