3d2bc0cfb529ca98430e0eb25f62dc0107b8af43
Nick Papior Andersen <nickpapior@gmail.com>
2014-09-23 21:02:26 +0000

    Changed zlib to compile both static and dynamic library

--- libs/zlib.bash
+++ libs/zlib.bash
@@ -7,11 +7,10 @@ pack_set --install-query $(pack_get --LD)/libz.a
 
 # Install commands that it should run
 pack_set --command "./configure" \
-    --command-flag "--prefix $(pack_get --prefix)" \
-    --command-flag "--static"
+    --command-flag "--prefix $(pack_get --prefix)"
 
 # Make commands
 pack_set --command "make $(get_make_parallel)"
-pack_set --command "make check > tmp.test 2>&1"
+pack_set --command "make test > tmp.test 2>&1"
 pack_set --command "make install"
 pack_set_mv_test tmp.test
