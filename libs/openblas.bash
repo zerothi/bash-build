bc3e1f36fdfaee0c7a435161c7ecea112948cd7a
Nick Papior Andersen <nickpapior@gmail.com>
2014-09-25 09:40:06 +0200

    Removed OpenBLAS and atlas from Hashwell installation

--- libs/openblas.bash
+++ libs/openblas.bash
@@ -6,6 +6,9 @@ pack_set -s $IS_MODULE
 
 pack_set --install-query $(pack_get --LD)/libopenblas.a
 
+# Currently this version does not support Haswell
+pack_set --host-reject n-62-17-44
+
 # NO_LAPACK=1 means that we do not need -lgfortran
 pack_set --command "sed -i -s -e 's:-lgfortran::g' Makefile.system f_check"
 
