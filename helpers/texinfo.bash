0ff37e7f31196dab479212f2e40472f1c468d4fb
Nick Papior Andersen <nickpapior@gmail.com>
2015-02-25 20:40:31 +0100

    install-query updated for texinfo

--- helpers/texinfo.bash
+++ helpers/texinfo.bash
@@ -2,7 +2,7 @@ add_package --build generic http://ftp.gnu.org/gnu/texinfo/texinfo-5.2.tar.xz
 
 pack_set -s $MAKE_PARALLEL
 
-pack_set --install-query $(pack_get --prefix build-tools)/bin/texinfo
+pack_set --install-query $(pack_get --prefix build-tools)/bin/makeinfo
 
 # Install commands that it should run
 pack_set --command "./configure" \
