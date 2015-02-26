0b5d405cd76c77b9909d7c4c4fbfaeb78306258d
Nick Papior Andersen <nickpapior@gmail.com>
2015-02-26 09:11:40 +0000

    Forgot to add sqlite and pysqlite to the installation.

---
+++ helpers/sqlite.bash
@@ -0,0 +1,15 @@
+v=3.8.8.3
+add_package --package sqlite --build generic --version $v \
+    http://www.sqlite.org/2015/sqlite-autoconf-${v//./0}.tar.gz
+
+pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR
+
+pack_set --install-query $(pack_get --prefix)/bin/sqlite3
+
+# Install commands that it should run
+pack_set --command "../configure" \
+    --command-flag "--prefix $(pack_get --prefix)"
+
+# Make commands
+pack_set --command "make $(get_make_parallel)"
+pack_set --command "make install"
---
+++ python/pysqlite.bash
@@ -0,0 +1,19 @@
+add_package https://pypi.python.org/packages/source/p/pysqlite/pysqlite-2.6.3.tar.gz
+
+pack_set --module-requirement sqlite
+pack_set --install-query $(pack_get --prefix $(get_parent))/lib/python$pV/site-packages/$(pack_get --package)-$(pack_get --version)-py$pV.egg
+
+# Create the sqlite setup.cfg
+file=setup.cfg
+pack_set --command "echo '# Setup for pysqlite' > $file"
+pack_set --command "sed -i '$ a\
+[build_ext]\n\
+include_dirs = $(pack_get --prefix sqlite)/include\n\
+library_dirs = $(pack_get --LD sqlite)\n\
+#runtime_library_dirs = $(pack_get --LD sqlite)\n\
+rpath = $(pack_get --LD sqlite)\n\
+libraries = sqlite3\n\
+define = SQLITE_OMIT_LOAD_EXTENSION\n' $file"
+
+pack_set --command "$(get_parent_exec) setup.py install" \
+    --command-flag "--prefix=$(pack_get --prefix $(get_parent))"
