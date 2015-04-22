ecf82f2c912eed715f2b9707aa0633492a19abf3
Nick Papior Andersen <nickpapior@gmail.com>
2015-04-22 19:52:17 +0200

    Siesta arch updated

--- applications/siesta-scf.bash
+++ applications/siesta-scf.bash
@@ -53,7 +53,7 @@ fi
 pack_set --command "sed -i '1 a\
 .SUFFIXES:\n\
 .SUFFIXES: .f .F .o .a .f90 .F90\n\
-SIESTA_ARCH=x86_64-linux-Intel\n\
+SIESTA_ARCH=x86_64-linux-$(get_c)\n\
 \n\
 FPP=mpif90\n\
 FPP_OUTPUT= \n\
