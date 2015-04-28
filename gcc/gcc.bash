34050dfbb2bb0f82fc2ede3786898df0b7954a5f
Nick Papior Andersen <nickpapior@gmail.com>
2015-04-28 16:07:50 +0200

    Forgot to add gcc sourcing

---
+++ gcc/gcc.bash
@@ -0,0 +1,13 @@
+source gcc/4.9.2/gmp.bash
+source gcc/4.9.2/mpfr.bash
+source gcc/4.9.2/mpc.bash
+source gcc/4.9.2/isl.bash
+source gcc/4.9.2/cloog.bash
+source gcc/4.9.2/gcc.bash
+
+source gcc/5.1.0/gmp.bash
+source gcc/5.1.0/mpfr.bash
+source gcc/5.1.0/mpc.bash
+source gcc/5.1.0/isl.bash
+source gcc/5.1.0/gcc.bash
+
