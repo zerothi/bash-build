034b826df79c7b17fa441fe46005f69a947b4dfb
Nick Papior Andersen <nickpapior@gmail.com>
2015-05-10 18:44:11 +0200

    Updated MUMPS installation to both install OpenMP and non-threaded version

--- libs/mumps.bash
+++ libs/mumps.bash
@@ -4,7 +4,7 @@ add_package --package mumps \
 
 pack_set -s $IS_MODULE
 
-pack_set --install-query $(pack_get --LD)/libmumps_common.a
+pack_set --install-query $(pack_get --LD)/libmumps_common_omp.a
 
 if [ $(vrs_cmp $v 5.0.0) -ge 0 ]; then
     parmetisV=parmetis
@@ -19,8 +19,8 @@ pack_set --command "echo '# Makefile for easy installation ' > Makefile.inc"
 if $(is_c intel) ; then
     tmp_flag="-nofor-main"
     pack_set --command "sed -i '1 a\
-SCALAP = $MKL_LIB -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -mkl=parallel \n\
-LIBBLAS = $MKL_LIB -lmkl_blas95_lp64 -mkl=parallel \n' Makefile.inc"
+SCALAP = $MKL_LIB -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -mkl=sequential \n\
+LIBBLAS = $MKL_LIB -lmkl_blas95_lp64 -mkl=sequential \n' Makefile.inc"
 
 else
 
@@ -94,9 +94,9 @@ CDEFS   = -DAdd_ \n\
 #CDEFS   = -D \n\
 \n\
 #Begin Optimized options\n\
-OPTF    = $FCFLAGS -O -DALLOW_NON_INIT $tmp_flag $FLAG_OMP\n\
-OPTL    = $FCFLAGS -O $tmp_flag $FLAG_OMP\n\
-OPTC    = $CFLAGS -O $FLAG_OMP\n\
+OPTF    = $FCFLAGS -O -DALLOW_NON_INIT $tmp_flag \n\
+OPTL    = $FCFLAGS -O $tmp_flag \n\
+OPTC    = $CFLAGS -O \n\
 \n\
 ##INCS = \$(INCSEQ) \n\
 ##LIBS = \$(LIBSEQ) \n\
@@ -112,4 +112,33 @@ pack_set --command "cp include/*.h $(pack_get --prefix)/include/"
 pack_set --command "mkdir -p $(pack_get --LD)"
 pack_set --command "cp lib/lib*.a $(pack_get --LD)/"
 
+# Make clean and create threaded
+pack_set --command "make clean"
+if $(is_c intel) ; then
+    pack_set --command "sed -i -e 's:mkl=sequential:mkl=parallel:g' Makefile.inc"
+
+else
+
+    for la in $(choice linalg) ; do
+	if [ $(pack_installed $la) -eq 1 ]; then
+	    if [ "x$la" == "xopenblas" ]; then
+		pack_set --command "sed -i -e 's:lopenblas:lopenblas_omp:g' Makefile.inc"
+	    fi
+	fi
+    done
+fi
+
+pack_set --command "sed -i '$ a\
+CDEFS += -DMUMPS_OPENMP\n\
+OPTF += $FLAG_OMP\n\
+OPTL += $FLAG_OMP\n\
+OPTC += $FLAG_OMP\n' Makefile.inc"
+
+# Make commands
+pack_set --command "make $(get_make_parallel) alllib"
+pack_set --command "cp include/*.h $(pack_get --prefix)/include/"
+pack_set --command "cd lib"
+pack_set --command 'for l in lib*.a ; do cp $l $(pack_get --LD)/${l//.a/_omp.a} ; done'
+
 done
+
