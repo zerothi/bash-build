81392330e33eb2ae0f788f1dc5aa774c66cb47dc
Nick Papior Andersen <nickpapior@gmail.com>
2015-05-28 17:18:30 +0200

    Streamlined the build-tools package, moved binutils to build-tools (why not?)

--- helpers/automake.bash
+++ helpers/automake.bash
@@ -3,6 +3,7 @@ add_package --build generic http://ftp.gnu.org/gnu/automake/automake-1.14.tar.gz
 pack_set -s $MAKE_PARALLEL
 
 pack_set --module-requirement build-tools
+pack_set --prefix $(pack_get --prefix build-tools)
 
 p_V=$(pack_get --version)
 c_V=`automake --version 2> /dev/null | head -1 | awk '{print $4}'`
@@ -11,11 +12,11 @@ if [ $(vrs_cmp $c_V $p_V) -eq 1 ]; then
     pack_set --host-reject "$(get_hostname)"
 fi
 
-pack_set --install-query $(pack_get --prefix build-tools)/bin/automake
+pack_set --install-query $(pack_get --prefix)/bin/automake
 
 # Install commands that it should run
 pack_set --command "./configure" \
-    --command-flag "--prefix $(pack_get --prefix build-tools)"
+    --command-flag "--prefix $(pack_get --prefix)"
 
 # Make commands
 pack_set --command "make $(get_make_parallel)"
--- helpers/m4.bash
+++ helpers/m4.bash
@@ -3,6 +3,7 @@ add_package --build generic http://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.gz
 pack_set -s $MAKE_PARALLEL
 
 pack_set --module-requirement build-tools
+pack_set --prefix $(pack_get --prefix build-tools)
 
 p_V=$(pack_get --version)
 c_V=`m4 --version 2>/dev/null| head -1 | awk '{print $4}'`
@@ -11,12 +12,12 @@ if [ $(vrs_cmp $c_V $p_V) -eq 1 ]; then
     pack_set --host-reject "$(get_hostname)"
 fi
 
-pack_set --install-query $(pack_get --prefix build-tools)/bin/m4
+pack_set --install-query $(pack_get --prefix)/bin/m4
 
 # Install commands that it should run
 pack_set --command "./configure" \
     --command-flag "--enable-c++" \
-    --command-flag "--prefix $(pack_get --prefix build-tools)"
+    --command-flag "--prefix $(pack_get --prefix)"
 
 # Make commands
 pack_set --command "make $(get_make_parallel)"
