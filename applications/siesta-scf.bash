dd4cac8fb639571f72a9de400653bc6a866b066b
Nick Papior Andersen <nickpapior@gmail.com>
2015-06-10 09:18:33 +0200

    scf: removed atom compilation due to the trunk merge

--- applications/siesta-scf.bash
+++ applications/siesta-scf.bash
@@ -1,4 +1,4 @@
-for v in 878 ; do
+for v in 880 ; do
 
 add_package http://www.student.dtu.dk/~nicpa/packages/siesta-scf-$v.tar.bz2
 
@@ -290,10 +290,10 @@ pack_set --command "$FC $FCFLAGS vpsa2bin.f -o $(pack_get --prefix)/bin/vpsa2bin
 pack_set --command "$FC $FCFLAGS vpsb2asc.f -o $(pack_get --prefix)/bin/vpsb2asc"
 
 # The atom program for creating the pseudos
-pack_set --command "cd ../Pseudo/atom"
-make_files atm
+#pack_set --command "cd ../Pseudo/atom"
+#make_files atm
 
-pack_set --command "cd ../../Obj"
+pack_set --command "cd ../Obj"
 
 # Compile the 3m equivalent versions, if applicable
 tmp=0
