1fdaa95daf52957f3f3682d0c780ae70548538ab
Nick Papior Andersen <nickpapior@gmail.com>
2015-08-13 21:35:53 +0000

    updated povray to 3.7.0.0, much better implementation, faster

--- applications/povray.bash
+++ applications/povray.bash
@@ -1,7 +1,7 @@
-# apt-get libpng
-add_package \
-    --build generic \
-    http://www.povray.org/ftp/pub/povray/Old-Versions/Official-3.62/Unix/povray-3.6.1.tar.bz2
+# apt-get libpng libjpeg-dev libtiff5-dev libpng12-dev
+add_package --archive povray-3.7.0.0.tar.gz \
+    https://github.com/POV-Ray/povray/archive/v3.7.0.0.tar.gz
+#    http://www.povray.org/ftp/pub/povray/Old-Versions/Official-3.62/Unix/povray-3.6.1.tar.bz2
 
 pack_set -s $IS_MODULE -s $CRT_DEF_MODULE
 
@@ -9,9 +9,22 @@ pack_set --install-query $(pack_get --prefix)/bin/povray
 
 pack_set --module-opt "--lua-family povray"
 
-pack_set --command "./configure" \
+pack_set --mod-req zlib
+pack_set --mod-req boost
+
+pack_set --command "cd unix"
+
+# create configure
+pack_set --command "module load build-tools"
+pack_set --command "./prebuild.sh"
+# This fixes build on debian >=7
+pack_set --command "cd .. ; automake --add-missing ; cd unix"
+pack_set --command "./prebuild.sh"
+pack_set --command "cd .."
+
+pack_set --command "./configure --with-boost-libdir=$(pack_get -LD boost)" \
 	--command-flag "COMPILED_BY='Nick Papior Andersen <nickpapior@gmail.com>'" \
-	--command-flag "--prefix=$(pack_get --prefix)"
+	--command-flag "--prefix=$(pack_get --prefix) LIBS=-lboost_system"
 
 pack_set --command "make"
 pack_set --command "make install"
