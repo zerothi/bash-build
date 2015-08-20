8fb2f3680c35ef69d213ea2c8934d233e7e867f1
Nick Papior Andersen <nickpapior@gmail.com>
2015-08-20 10:42:27 +0200

    +hwloc and lapack

--- libs/lapack.bash
+++ libs/lapack.bash
@@ -2,7 +2,7 @@
 bl=$1
 shift
 
-add_package --package lapack-$bl http://www.student.dtu.dk/~nicpa/packages/lapack-1545.tar.gz
+add_package --package lapack-$bl http://www.student.dtu.dk/~nicpa/packages/lapack-1576.tar.bz2
 
 pack_set -s $MAKE_PARALLEL
 
