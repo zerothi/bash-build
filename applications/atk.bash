bd377d12474f26fbbc9d87e0a3d91cd43f504637
Nick Papior <nickpapior@gmail.com>
2015-08-24 08:40:26 +0200

    +ATK license server

--- applications/atk.bash
+++ applications/atk.bash
@@ -7,8 +7,6 @@ pack_set -s $IS_MODULE -s $CRT_DEF_MODULE
 
 pack_set --install-query $(pack_get --prefix)/bin/atkpython
 
-pack_set $(list -p '--host-reject ' zero ntch)
-
 pack_set --module-opt "--lua-family ATK"
 
 # Make the binary x
@@ -17,3 +15,14 @@ pack_set --command "$(build_get --archive-path)/$(pack_get --archive)" \
     --command-flag "--prefix $(pack_get --prefix)" \
     --command-flag "--mode unattended --license_file non-existing" \
     --command-flag "--license_configuration floating"
+
+# Define license servers etc.
+if $(is_host n-) ; then
+    # HPC, server @ QUANTUM_LICENSE_PATH=[6220]@license1.cc.dtu.dk
+    pack_set --module-opt "--set-ENV QUANTUM_LICENSE_PATH=[6220]@license1.cc.dtu.dk"
+    pack_set --module-opt "--set-ENV QUANTUM_AUTOMATIC_SERVER_DISCOVERY=0"
+fi
+
+# Add license path to PATH
+pack_set --module-opt "--prepend-ENV PATH=$(pack_get --prefix)/license"
+
