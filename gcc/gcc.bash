cefdf5a7bba34cf3222f4c92e9388b619c2d5b37
Nick Papior <nickpapior@gmail.com>
2015-09-10 15:03:34 +0000

    Fixed paths for gcc2

--- gcc/gcc.bash
+++ gcc/gcc.bash
@@ -5,9 +5,9 @@ source_pack gcc/4.9.2/isl.bash
 source_pack gcc/4.9.2/cloog.bash
 source_pack gcc/4.9.2/gcc.bash
 
-source_pack gcc/5.1.0/gmp.bash
-source_pack gcc/5.1.0/mpfr.bash
-source_pack gcc/5.1.0/mpc.bash
-source_pack gcc/5.1.0/isl.bash
-source_pack gcc/5.1.0/gcc.bash
+source_pack gcc/5/gmp.bash
+source_pack gcc/5/mpfr.bash
+source_pack gcc/5/mpc.bash
+source_pack gcc/5/isl.bash
+source_pack gcc/5/gcc.bash
 
