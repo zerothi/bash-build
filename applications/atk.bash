1c5abf4040b6133923eb64249f0907c069ffe4bf
Nick Papior <nickpapior@gmail.com>
2015-09-25 10:24:47 +0200

    fixed ATK

--- applications/atk.bash
+++ applications/atk.bash
@@ -11,21 +11,30 @@ pack_set --module-opt "--lua-family ATK"
 
 # Make the binary x
 pack_cmd "chmod u+x $(build_get --archive-path)/$(pack_get --archive)"
-pack_cmd "$(build_get --archive-path)/$(pack_get --archive)" \
-     "--prefix $(pack_get --prefix)" \
-     "--mode unattended --license_file $(build_get --archive-path)/201500982_DTU_A01693.lic‎" 
 
-#     "--license_configuration floating"
+if [[ $(vrs_cmp $v 2015) -ge 0 ]]; then
+    pack_cmd "$(build_get --archive-path)/$(pack_get --archive)" \
+	"--prefix $(pack_get --prefix)"
+else
+    pack_cmd "$(build_get --archive-path)/$(pack_get --archive)" \
+	"--prefix $(pack_get --prefix)" \
+	"--mode unattended --license_file non-existing" \
+	"--license_configuration floating"
+fi
+
+atklic=201500982_DTU_A01693.lic‎
 
 # Define license servers etc.
 if $(is_host n-) ; then
     # HPC, server @ QUANTUM_LICENSE_PATH=[6220]@license1.cc.dtu.dk
     pack_set --module-opt "--set-ENV QUANTUM_AUTOMATIC_SERVER_DISCOVERY=0"
-    pack_set --module-opt "--set-ENV QUANTUM_LICENSE_PATH=\[6220\]\@license1.cc.dtu.dk"
+    pack_set --module-opt "--set-ENV QUANTUM_LICENSE_PATH=\[6220\]\@license1.cc.dtu.dk:$(pack_get --prefix)/license/$atklic"
 fi
 
 # Add license path to PATH
 pack_set --module-opt "--prepend-ENV PATH=$(pack_get --prefix)/license"
 # Copy license
-pack_cmd "cp $(build_get --archive-path)/201500982_DTU_A01693.lic‎ $(pack_get --prefix)/license/"
-#pack_cmd "chmod a+r $(pack_get --prefix)/license/201500982_DTU_A01693.lic"
+pack_cmd "cp $(build_get --archive-path)/$atklic‎ $(pack_get --prefix)/license/"
+#pack_cmd "chmod a+r $(pack_get --prefix)/license/$atklic"
+
+unset atklic
