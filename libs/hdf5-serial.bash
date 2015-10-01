7a04b23fef114dc307b748a6c4727a1e3852c9bc
Nick Papior <nickpapior@gmail.com>
2015-10-01 22:10:51 +0000

    Updated hdf5 to 1.8.15

--- libs/hdf5-serial.bash
+++ libs/hdf5-serial.bash
@@ -1,9 +1,10 @@
 # Then install HDF5
-for p in 1.8.14 ; do
+for p in 1.8.15 ; do
 
 add_package \
     --package hdf5-serial \
-    http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-$p/src/hdf5-$p.tar.gz
+    --directory hdf5-$p --version $p \
+    http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-$p-patch1.tar.bz2
 
 pack_set -s $BUILD_DIR -s $MAKE_PARALLEL -s $IS_MODULE
 
