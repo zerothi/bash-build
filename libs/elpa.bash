825cc85839b7ead142b493c5da5163cb17a77317
Nick Papior <nickpapior@gmail.com>
2015-10-01 22:52:14 +0000

    enabled elpa library compilation

--- libs/elpa.bash
+++ libs/elpa.bash
@@ -1,40 +1,33 @@
-return 0
 v=2015.05.001
 add_package http://elpa.rzg.mpg.de/elpa-$v.tar.gz
 
 pack_set -s $IS_MODULE -s $BUILD_DIR
 
+pack_set --install-query $(pack_get --LD)/libelpa.a
+
 pack_set --module-requirement mpi
 
 if $(is_c intel) ; then
-    tmp="-dir=$MKL_PATH/lib/intel64"
+    tmp="-lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -lmkl_lapack95_lp64 -lmkl_blas95_lp64"
+    tmp="$tmp -lmkl_intel_lp64 -lmkl_core -lmkl_sequential"
 
 else
     pack_set --module-requirement scalapack
+    tmp="-lscalapack"
     for la in $(choice linalg) ; do
 	if [[ $(pack_installed $la) -eq 1 ]]; then
 	    pack_set --module-requirement $la
-	    tmp=
 	    [[ "x$la" == "xatlas" ]] && \
-		tmp="-lf77blas -lcblas"
+		tmp="$tmp -lf77blas -lcblas"
 	    tmp="$tmp -l$la"
-	    tmp="--with-lapack-lib='-llapack' --with-blas-lib='$tmp'"
 	    break
 	fi
     done
-    tmp="$tmp --with-scalapack-dir=$(pack_get --prefix scalapack)"
 fi
 
+# We cannot use OpenMP threading as it requires sequential BLAS
 
-pack_set --install-query $(pack_get --LD)/libelpa.a
-
-pack_cmd "../configure" \
-	 "CC='$MPICC' CFLAGS='$CFLAGS'" \
-	 "CXX='$MPICXX' CXXFLAGS='$CFLAGS'" \
-	 "FC='$MPIF90' FCFLAGS='$FCFLAGS'" \
-	 "F77='$MPIF77' FFLAGS='$FFLAGS'" \
-	 "F90='$MPIF90'" \
-	 "--enable-shared" \
+pack_cmd "../configure CC='$MPICC' CXX='$MPICXX' FC='$MPIFC' SCALAPACK_LDFLAGS='$tmp'" \
 	 "--prefix=$(pack_get --prefix)"
 
 pack_cmd "make $(get_make_parallel)"
