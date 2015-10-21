86449e5241a7fbc71ff27a63eca2401282a120da
Nick Papior <nickpapior@gmail.com>
2015-10-21 17:10:28 +0000

    +py-netcdf
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/netcdf4.bash
+++ python/netcdf4.bash
@@ -1,4 +1,4 @@
-v=1.2.0
+v=1.2.1
 add_package --version $v --package netcdf4py \
     --archive netcdf4-python-${v}rel.tar.gz \
     https://github.com/Unidata/netcdf4-python/archive/v${v}rel.tar.gz
