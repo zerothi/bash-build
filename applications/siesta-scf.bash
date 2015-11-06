c89642f1731be46540814809d3b4d0cf5237c0c7
Nick Papior <nickpapior@gmail.com>
2015-11-06 22:42:18 +0100

    fixed kwant installation and +ts-scf
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- applications/siesta-scf.bash
+++ applications/siesta-scf.bash
@@ -1,4 +1,4 @@
-for v in 1046 ; do
+for v in 1048 ; do
 
 add_package http://www.student.dtu.dk/~nicpa/packages/siesta-scf-$v.tar.bz2
 
--- python/kwant.bash
+++ python/kwant.bash
@@ -5,6 +5,7 @@ add_package http://downloads.kwant-project.org/kwant/kwant-1.1.1.tar.gz
 pack_set -s $IS_MODULE -s $PRELOAD_MODULE
 
 pack_set --install-query $(pack_get --LD)/python$pV/site-packages/site.py
+pack_cmd "mkdir -p $(pack_get --LD)/python$pV/site-packages"
 
 pack_set --module-opt "--lua-family kwant"
 
