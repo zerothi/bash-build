28e4ffe372f37df02a3951f1aff4207a8080f0a5
Nick Papior <nickpapior@gmail.com>
2015-11-07 00:28:15 +0100

    fixed openssl compilation
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- helpers/openssl.bash
+++ helpers/openssl.bash
@@ -8,7 +8,7 @@ pack_set --mod-req gen-zlib
 pack_set --install-query $(pack_get --prefix)/lib/libssl.a
 
 # Install commands that it should run
-pack_cmd "./config --prefix=$(pack_get --prefix)" \
+pack_cmd "./config -fPIC --prefix=$(pack_get --prefix)" \
 	"--openssldir=$(pack_get --prefix)/openssl"
 
 # Make commands
