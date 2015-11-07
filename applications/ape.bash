fc729998a9407c34c5d2133af9611a1107864302
Nick Papior <nickpapior@gmail.com>
2015-11-07 21:34:55 +0100

    fix ape
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- applications/ape.bash
+++ applications/ape.bash
@@ -1,6 +1,8 @@
 for v in 2.2.1 ; do
 add_package http://www.tddft.org/programs/APE/sites/default/files/ape-$v.tar.gz
 
+pack_set -s $BUILD_DIR
+
 pack_set --install-query $(pack_get --prefix)/bin/ape
 
 pack_set --module-requirement gsl \
@@ -12,14 +14,14 @@ pack_set --module-opt "--lua-family ape"
 pack_cmd "unset CFLAGS"
 pack_cmd "unset FCFLAGS"
 
-pack_cmd "./configure" \
+pack_cmd "../configure" \
      "--with-gsl-prefix=$(pack_get --prefix gsl)" \
      "--with-libxc-prefix=$(pack_get --prefix libxc)" \
      "--prefix=$(pack_get --prefix)"
 
 # Make commands
 pack_cmd "make $(get_make_parallel)"
-pack_cmd "make check 2>&1 tmp.test ; echo FORCED"
+pack_cmd "make check 2>&1 > tmp.test ; echo FORCED"
 pack_cmd "make install"
 pack_set_mv_test tmp.test
 
