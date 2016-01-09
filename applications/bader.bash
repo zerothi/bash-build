354e48096586189a1d309c857b8faebbdd386160
Nick Papior <nickpapior@gmail.com>
2016-01-09 09:43:22 +0100

    +bader
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- applications/bader.bash
+++ applications/bader.bash
@@ -1,4 +1,4 @@
-add_package --version 0.28a \
+add_package --version 0.95 \
     http://theory.cm.utexas.edu/henkelman/code/bader/download/bader.tar.gz
 
 pack_set -s $MAKE_PARALLEL
@@ -6,18 +6,9 @@ pack_set -s $MAKE_PARALLEL
 pack_set --install-query $(pack_get --prefix)/bin/bader
 
 file=Makefile
-pack_cmd "echo '.SUFFIXES: .f90' > $file"
-
-pack_cmd "sed -i '1 a\
-FC = $FC \n\
-FFLAGS = ${FCFLAGS//-O3/-O2} \n\
-LINK = \n\
-OBJS = kind_mod.o matrix_mod.o ions_mod.o options_mod.o charge_mod.o \
-chgcar_mod.o cube_mod.o io_mod.o bader_mod.o voronoi_mod.o multipole_mod.o main.o \n\
-%.o %.mod: %.f90\n\
-\t\$(FC) \$(FFLAGS) -c \$\*.f90\n\
-bader: \$(OBJS)\n\
-\t\$(FC) \$(LINK) -o bader \$(OBJS)' $file"
+pack_cmd "cp makeflie.lnx_ifort Makefile"
+pack_cmd "sed -i -e 's/\(FC[[:space:]]*=\).*/\1 $FC/' $file"
+pack_cmd "sed -i -e 's/\(FFLAGS[[:space:]]*=\).*/\1 $FFLAGS/' $file"
 
 # Make commands
 pack_cmd "make bader"
