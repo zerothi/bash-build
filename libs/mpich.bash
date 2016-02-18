7f88261c347b2d5209a14742036c435b55a2c655
Nick Papior <nickpapior@gmail.com>
2016-02-18 11:07:46 +0100

    initial fix for mpich, needs to be checked
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/mpich.bash
+++ libs/mpich.bash
@@ -1,39 +1,47 @@
 v=3.2
-add_package http://www.mpich.org/static/downloads/$v/mpich-$v.tar.gz
 
-pack_set -s $BUILD_DIR -s $MAKE_PARALLEL -s $IS_MODULE
+# install HYDRA
+add_package http://www.mpich.org/static/downloads/$v/hydra-$v.tar.gz
 
-pack_set --install-query $(pack_get --prefix)/bin/mpiexec
+pack_set -s $BUILD_DIR -s $MAKE_PARALLEL
 
-pack_set --host-reject $(get_hostname)
+pack_set --mod-req hwloc
+
+# Fix hydra installation in the same directory as mpich
+tmp=$(pack_get --prefix)
+pack_set --prefix ${tmp//hydra/mpich}
+
+pack_set --install-query $(pack_get --prefix)/bin/hydra
 
 pack_cmd "unset F90"
 pack_cmd "unset F90FLAGS"
-pack_cmd "../configure" \
-	 "--prefix=$(pack_get --prefix)" \
-	 "--enable-fortran=all --enable-cxx" \
-	 "--enable-shared --enable-smpcoll"
+pack_cmd "../configure --prefix=$(pack_get --prefix)" \
+	 "--with-hwloc-prefix=$(pack_get --prefix hwloc)"
 
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make install"
 
+# Install mpich
+add_package http://www.mpich.org/static/downloads/$v/mpich-$v.tar.gz
 
-## Also install HYDRA
-add_package http://www.mpich.org/static/downloads/$v/hydra-$v.tar.gz
+pack_set -s $BUILD_DIR -s $MAKE_PARALLEL -s $IS_MODULE
 
-pack_set --prefix $(pack_get --prefix mpich)
-pack_set --mod-req mpich
-
-pack_set -s $BUILD_DIR -s $MAKE_PARALLEL
+pack_set --mod-req hydra
 
 pack_set --install-query $(pack_get --prefix)/bin/mpiexec
+
+tmp_flags=
+if [[ -d /usr/include/infiniband ]]; then
+    tmp_flags="$tmp_flags --with-ibverbs=/usr/include/infiniband"
+fi
 
 pack_cmd "unset F90"
 pack_cmd "unset F90FLAGS"
 pack_cmd "../configure" \
 	 "--prefix=$(pack_get --prefix)" \
 	 "--enable-fortran=all --enable-cxx" \
-	 "--enable-shared --enable-smpcoll"
+	 "--enable-shared --enable-smpcoll" \
+	 "--with-pm=hydra $tmp_flags"
 
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make install"
