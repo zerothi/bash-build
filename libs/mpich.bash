c19cc0d468b33c0e263698b77f3cbdbe2a00a59f
Nick Papior <nickpapior@gmail.com>
2016-03-08 01:06:15 +0100

    fixed mpich and mvapich
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/mpich.bash
+++ libs/mpich.bash
@@ -1,32 +1,10 @@
 v=3.2
 
-# install HYDRA
-add_package http://www.mpich.org/static/downloads/$v/hydra-$v.tar.gz
-
-pack_set -s $BUILD_DIR -s $MAKE_PARALLEL
-
-pack_set --mod-req hwloc
-
-# Fix hydra installation in the same directory as mpich
-tmp=$(pack_get --prefix)
-pack_set --prefix ${tmp//hydra/mpich}
-
-pack_set --install-query $(pack_get --prefix)/bin/mpiexec.hydra
-
-pack_cmd "unset F90"
-pack_cmd "unset F90FLAGS"
-pack_cmd "../configure --prefix=$(pack_get --prefix)" \
-	 "--with-hwloc-prefix=$(pack_get --prefix hwloc)"
-
-pack_cmd "make $(get_make_parallel)"
-pack_cmd "make install"
-
-# Install mpich
 add_package http://www.mpich.org/static/downloads/$v/mpich-$v.tar.gz
 
 pack_set -s $BUILD_DIR -s $MAKE_PARALLEL -s $IS_MODULE
 
-pack_set --mod-req hydra
+pack_set --mod-req hwloc
 
 pack_set --install-query $(pack_get --prefix)/bin/mpicc
 
@@ -44,5 +22,42 @@ pack_cmd "../configure" \
 	 "--enable-shared --enable-smpcoll" \
 	 "--with-pm=hydra $tmp_flags"
 
+# We first need to assert the postdeps are correct
+# Sadly this comes about in certain environments.
+# But I am not fully sure why it happens...
+for f in libtool config.lt config.status
+do
+    pack_cmd "sed -i -e 's/-l -l / /' $f"
+done
+
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make install"
+
+
+
+# install HYDRA
+# MPICH installs its own, minimal hydra.
+# However, here we would like to use hwloc
+add_package http://www.mpich.org/static/downloads/$v/hydra-$v.tar.gz
+
+pack_set -s $BUILD_DIR -s $MAKE_PARALLEL
+
+pack_set --mod-req mpich
+
+# Fix hydra installation in the same directory as mpich
+tmp=$(pack_get --prefix)
+pack_set --prefix $(pack_get --prefix mpich)
+
+pack_set --install-query $(pack_get --prefix)/custom.hydra
+
+pack_cmd "unset F90"
+pack_cmd "unset F90FLAGS"
+pack_cmd "../configure --prefix=$(pack_get --prefix)" \
+	 "--enable-fortran=all --enable-cxx" \
+	 "--enable-threads=runtime" \
+	 "--enable-shared $tmpflags" \
+	 "--with-hwloc-prefix=$(pack_get --prefix hwloc)"
+
+pack_cmd "make $(get_make_parallel)"
+pack_cmd "make install"
+pack_cmd "touch $(pack_get --prefix)/custom.hydra"
--- libs/mvapich.bash
+++ libs/mvapich.bash
@@ -7,6 +7,9 @@ pack_set --install-query $(pack_get --prefix)/bin/mpicc
 tmp_flags=
 if [[ -d /usr/include/infiniband ]]; then
     tmp_flags="$tmp_flags --with-ibverbs=/usr/include/infiniband"
+else
+    # MVAPICH requires infiniband
+    pack_set --host-reject $(get_hostname)
 fi
 
 pack_cmd "unset F90"
@@ -17,6 +20,14 @@ pack_cmd "../configure" \
 	 "--enable-threads=runtime" \
 	 "--enable-shared --enable-smpcoll" \
 	 "--with-pm=hydra $tmp_flags"
+
+# We first need to assert the postdeps are correct
+# Sadly this comes about in certain environments.
+# But I am not fully sure why it happens...
+for f in libtool config.lt config.status
+do
+    pack_cmd "sed -i -e 's/-l -l / /' $f"
+done
 
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make install"
