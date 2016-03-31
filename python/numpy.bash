3f325441e16126e8f27354712e8f3c447c444e97
Nick Papior <nickpapior@gmail.com>
2016-03-31 15:14:27 +0200

    numpy now relies on cython
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/numpy.bash
+++ python/numpy.bash
@@ -9,7 +9,7 @@ if [[ "x${pV:0:1}" == "x3" ]]; then
 else
     pack_set --install-query $(pack_get --prefix)/bin/f2py
 fi
-pack_set $(list -p '-mod-req ' fftw-2 fftw-3 umfpack)
+pack_set $(list -p '-mod-req ' fftw-2 fftw-3 umfpack cython)
 
 # For future maybe this flag is important: NPY_SEPARATE_COMPILATION=0
 
