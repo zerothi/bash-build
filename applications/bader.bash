383035ef377dca796a25aeddd34be7f4c76b8043
Nick Papior <nickpapior@gmail.com>
2016-04-06 11:37:43 +0200

    fix bader (no static)
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- applications/bader.bash
+++ applications/bader.bash
@@ -6,9 +6,10 @@ pack_set -s $MAKE_PARALLEL
 pack_set --install-query $(pack_get --prefix)/bin/bader
 
 file=Makefile
-pack_cmd "cp makefile.lnx_ifort Makefile"
+pack_cmd "cp makefile.lnx_ifort $file"
 pack_cmd "sed -i -e 's/\(FC[[:space:]]*=\).*/\1 $FC/' $file"
 pack_cmd "sed -i -e 's/\(FFLAGS[[:space:]]*=\).*/\1 $FFLAGS/' $file"
+pack_cmd "sed -i -e 's/\(LINK[[:space:]]*=\).*/\1 /' $file"
 
 # Make commands
 pack_cmd "make bader"
