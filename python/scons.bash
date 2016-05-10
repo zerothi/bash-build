26a1809da87ea3b52a74a33609ac8fa153fb5d07
Nick Papior <nickpapior@gmail.com>
2016-05-10 09:29:45 +0200

    fixed scons for py3
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/scons.bash
+++ python/scons.bash
@@ -1,10 +1,11 @@
-[ "x${pV:0:1}" == "x3" ] && return 0
 
 add_package http://prdownloads.sourceforge.net/scons/scons-2.3.2.tar.gz
 
 pack_set -s $IS_MODULE
 
 pack_set --install-query $(pack_get --prefix)/bin/scons
+
+[ "x${pV:0:1}" == "x3" ] && pack_set --host-reject $(get_hostname)
 
 # Install commands that it should run
 pack_cmd "$(get_parent_exec) setup.py build"
