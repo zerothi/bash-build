6ff3861c367c2ed9dc72b1e3fa690c8a8fe9e24c
Nick Papior <nickpapior@gmail.com>
2016-05-14 15:26:08 +0200

    added tests for gcc 6
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- gcc/6/gcc.bash
+++ gcc/6/gcc.bash
@@ -23,9 +23,9 @@ pack_cmd "../configure" \
 # Make commands
 pack_cmd "make BOOT_LDFLAGS='$(list --LD-rp gmp[$gmp_v] mpfr[$mpfr_v] mpc[$mpc_v] isl[$isl_v])' $(get_make_parallel)"
 # make check requires autogen installed
-#pack_cmd "make check > tmp.test 2>&1"
+pack_cmd "make -k check > tmp.test 2>&1"
 pack_cmd "make install"
-#pack_set_mv_test tmp.test
+pack_set_mv_test tmp.test
 
 # Add to LD_LIBRARY_PATH, this ensures that at least 
 # these libraries always will be present in LD
