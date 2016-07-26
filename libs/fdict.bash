2b9ef98e37674270641faa99f261fbc1e3c85882
Nick Papior <nickpapior@gmail.com>
2016-07-26 10:13:14 +0200

    updated fdict
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/fdict.bash
+++ libs/fdict.bash
@@ -1,4 +1,4 @@
-v=0.3
+v=0.4
 add_package \
     --archive fdict-$v.tar.gz \
     https://github.com/zerothi/fdict/archive/$v.tar.gz
@@ -8,30 +8,20 @@ pack_set -s $IS_MODULE
 pack_set --install-query $(pack_get --prefix)/lib/libvardict.a
 
 # Create the arch-make file
-file=arch.make
+file=setup.make
 pack_cmd "echo '# Hello' > $file"
 pack_cmd "sed -i '1 a\
 FC = $FC\n\
 FC_SERIAL = $FC\n\
 FFLAGS = $FCFLAGS\n\
 AR = $AR\n\
-.F90.o:\n\
-\t\$(FC) -c \$(INC) \$(FFLAGS) \$(FPPFLAGS) \$< \n\
-.f90.o:\n\
-\t\$(FC) -c \$(INC) \$(FFLAGS) \$< \n\
 ' $file"
 
 # Make commands
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make test > tmp.test 2>&1 ; echo 'Fake success'"
 pack_set_mv_test tmp.test
-pack_cmd "mkdir -p $(pack_get --LD)"
-pack_cmd "mkdir -p $(pack_get --prefix)/include"
-pack_cmd "cp libvardict.a $(pack_get --LD)/"
-pack_cmd "cp *.mod $(pack_get --prefix)/include/"
-# We also copy the settings file
-# Mainly because it is needed for building ncdf library.
-pack_cmd "cp settings.sh $(pack_get --prefix)/include/"
+pack_cmd "make PREFIX=$(pack_get --prefix) install"
 
 
 
