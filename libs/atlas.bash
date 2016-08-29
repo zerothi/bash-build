93167a8926238bdf8f9acc750f26d4d0dc4135d9
Nick Papior <nickpapior@gmail.com>
2016-08-29 13:54:21 +0200

    updated script for atlas
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/atlas.bash
+++ libs/atlas.bash
@@ -6,7 +6,7 @@ if $(is_c gnu) ; then
     # we will prefer that
     tmp=
 fi
-if [[ $(vrs_cmp $v 3.10.2) -le 0 ]]; then
+if [[ $(vrs_cmp $v 3.10.3) -le 0 ]]; then
     add_package $tmp http://downloads.sourceforge.net/project/math-atlas/Stable/$v/atlas$v.tar.bz2
 else
     add_package $tmp http://www.student.dtu.dk/~nicpa/packages/atlas$v.tar.bz2
@@ -22,30 +22,46 @@ pack_set --lib -lf77blas -lcblas -latlas
 pack_set --lib[omp] -lf77blas -lcblas -latlas
 pack_set --lib[pt] -lptf77blas -lptcblas -lptatlas
 
+tmp=
 
 # Prepare the make file
-pack_cmd "sed -i -e 's/ThrChk[[:space:]]*=[[:space:]]*1/ThrChk = 0/' ../CONFIG/src/config.c"
+if [[ $(vrs_cmp $v 3.10.2) -le 0 ]]; then
+    pack_cmd "sed -i -e 's/ThrChk[[:space:]]*=[[:space:]]*1/ThrChk = 0/' ../CONFIG/src/config.c"
+    tmp="$tmp --with-netlib-lapack-tarfile=$(build_get --archive-path)/$(pack_get --archive lapack) -Ss flapack $(pack_get --LD lapack)/liblapack.a"
+else
+    # Do not do threadcheck
+    tmp="$tmp -Si cripple-perf 1"
+    tmp="$tmp --with-netlib-lapack-tarfile=$(build_get --archive-path)/$(pack_get --archive lapack)"
+fi
 
-tmp=
-if [[ $(vrs_cmp $v 3.10.2) -gt 0 ]]; then
-    tmp="--accel=0"
-    if $(is_host zero ntch) ; then
-        tmp="$tmp -m 2800"
-    fi
+if [[ $(vrs_cmp $v 3.10.2) -eq 0 ]]; then
+    tmp="$tmp --accel=0"
     pack_cmd "sed -i -e 's/int thrchk,/int thrchk=0,/' ../CONFIG/src/config.c"
 fi
 
+# Append the MHz
+tmp="$tmp -m $(get_Hz --MHz)"
+
+# Add fPIC for shared libraries
+tmp="$tmp --shared -Fa alg '-fPIC'"
+# Use 64 bit pointers
+tmp="$tmp -b 64"
+# Do not allow non-ieee breaks
+tmp="$tmp -Si ieee 1"
+# Tune lapack
+tmp="$tmp -Si latune 1"
+# Use parallel build
+tmp="$tmp -Ss pmake '\$(MAKE) $(get_make_parallel)'"
+# Use OpenMP for the threaded library
+tmp="$tmp -Si omp 1 -F alg $FLAG_OMP"
+
 # Configure command
 # -Fa alg: append to all compilers -fPIC
-pack_cmd "../configure -Fa alg '-fPIC'" \
-     "--with-netlib-lapack-tarfile=$(build_get --archive-path)/$(pack_get --archive lapack)" \
-	 "-Ss flapack $(pack_get --LD blas)/liblapack.a" \
+pack_cmd "../configure" \
 	 "--prefix=$(pack_get --prefix)" \
 	 "--incdir=$(pack_get --prefix)/include" \
 	 "--libdir=$(pack_get --LD)" \
-	 "-t $NPROCS --shared" \
-	 "-b 64 -Si latune 1 $tmp" \
-	 "-Ss pmake '\$(MAKE) $(get_make_parallel)'"
+	 "-t $NPROCS $tmp"
 
 pack_cmd "make"
 pack_cmd "make install"
@@ -63,7 +79,7 @@ add_hidden_package lapack-atlas/$v
 pack_set --installed $_I_REQ
 pack_set -mod-req atlas
 # Denote the default libraries
-# Note that this OpenBLAS compilation has lapack built-in
+# Note that this ATLAS compilation has lapack built-in
 pack_set --lib -llapack_atlas $(pack_get --lib atlas[$v])
 pack_set --lib[omp] -llapack_atlas $(pack_get --lib[omp] atlas[$v])
 pack_set --lib[pt] -llapack_atlas $(pack_get --lib[pt] atlas[$v])
