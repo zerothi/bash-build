642d1ae599d460a5aacd7ce0d1463ed6abf2ac5d
Nick Papior <nickpapior@gmail.com>
2016-08-31 21:14:43 +0200

    moved mpi4py installation to after numpy
    updated scipy
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/mpi4py.bash
+++ python/mpi4py.bash
@@ -3,6 +3,7 @@ add_package https://bitbucket.org/mpi4py/mpi4py/downloads/mpi4py-2.0.0.tar.gz
 pack_set -s $IS_MODULE
 
 pack_set --module-requirement mpi
+pack_set --module-requirement numpy
 
 pack_set --install-query $(pack_get --LD)/python$pV/site-packages/$(pack_get --alias)/__init__.py
 
@@ -10,10 +11,12 @@ pack_cmd "$(get_parent_exec) setup.py build"
 pack_cmd "$(get_parent_exec) setup.py install" \
     "--prefix=$(pack_get --prefix)"
 
-tmp="-v -f --no-numpy --exclude test_rma --exclude test_spawn"
+tmp="-v -f --exclude test_rma --exclude test_spawn"
 for i in 1 2 ; do
+    pack_cmd "mpirun -np $i $(get_parent_exec) test/runtests.py $tmp --no-numpy 2>&1 >> tmp$i.test"
     pack_cmd "mpirun -np $i $(get_parent_exec) test/runtests.py $tmp 2>&1 >> tmp$i.test"
     pack_set_mv_test tmp$i.test
     pack_cmd "mpirun -np $i $(get_parent_exec) test/runtests.py $tmp --thread-level funneled 2>&1 >> tmpthread$i.test"
+    pack_cmd "mpirun -np $i $(get_parent_exec) test/runtests.py $tmp --no-numpy --thread-level funneled 2>&1 >> tmpthread$i.test"
     pack_set_mv_test tmpthread$i.test
 done
