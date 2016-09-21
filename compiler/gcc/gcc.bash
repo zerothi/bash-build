e5972254ad876d69569dee587d3fd4405b93dbfc
Nick Papior <nickpapior@gmail.com>
2016-09-21 20:26:06 +0200

    moved compiler to common directory
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ compiler/gcc/gcc.bash
@@ -0,0 +1,19 @@
+#source_pack compiler/gcc/4/gmp.bash
+#source_pack compiler/gcc/4/mpfr.bash
+#source_pack compiler/gcc/4/mpc.bash
+#source_pack compiler/gcc/4/isl.bash
+#source_pack compiler/gcc/4/cloog.bash
+#source_pack compiler/gcc/4/gcc.bash
+
+source_pack compiler/gcc/5/gmp.bash
+source_pack compiler/gcc/5/mpfr.bash
+source_pack compiler/gcc/5/mpc.bash
+source_pack compiler/gcc/5/isl.bash
+source_pack compiler/gcc/5/gcc.bash
+
+source_pack compiler/gcc/6/gmp.bash
+source_pack compiler/gcc/6/mpfr.bash
+source_pack compiler/gcc/6/mpc.bash
+source_pack compiler/gcc/6/isl.bash
+source_pack compiler/gcc/6/gcc.bash
+
