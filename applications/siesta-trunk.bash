2b60333ae0c4be6fd5811899fd1fbb5994e52b64
Nick Papior <nickpapior@gmail.com>
2016-11-04 21:39:18 +0100

    updated siesta-trunk
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- applications/siesta-trunk.bash
+++ applications/siesta-trunk.bash
@@ -1,7 +1,7 @@
 # 507 pre SOC
 # 508 SOC
 # 510 Transiesta
-for v in 507 508 514 527 576 ; do
+for v in 507 508 514 527 582 ; do
 
 add_package --archive siesta-trunk-$v.tar.gz \
     --directory './~siesta-maint' \
