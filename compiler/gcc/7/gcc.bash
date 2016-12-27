903d300ec02ed4164a812d5817cde49e8be910cc
Nick Papior <nickpapior@gmail.com>
2016-12-27 13:53:50 +0100

    added gcc-7 (not released yet)
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ compiler/gcc/7/gcc.bash
@@ -0,0 +1,38 @@
+v=7.0.0
+add_package --build generic \
+	    http://ftp.download-by.net/gnu/gnu/gcc/gcc-$v/gcc-$v.tar.bz2
+
+pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR
+
+pack_set $(list --prefix '--module-requirement ' build-tools \
+		mpfr[$mpfr_v] mpc[$mpc_v] isl[$isl_v])
+
+pack_set --install-query $(pack_get --prefix)/bin/gcc
+
+languages="c,c++,fortran,objc,obj-c++"
+if ! $(is_host atto) ; then
+    languages="$languages,go"
+fi
+
+# Install commands that it should run
+pack_cmd "../configure" \
+	 "--prefix $(pack_get --prefix)" \
+	 "--with-gmp=$(pack_get --prefix gmp[$gmp_v])" \
+	 "--with-mpfr=$(pack_get --prefix mpfr[$mpfr_v])" \
+	 "--with-mpc=$(pack_get --prefix mpc[$mpc_v])" \
+	 "--with-isl=$(pack_get --prefix isl[$isl_v])" \
+	 "--enable-lto --enable-threads" \
+	 "--enable-stage1-languages=$languages" \
+	 "--with-multilib-list=m64"
+unset languages
+
+# Make commands
+pack_cmd "make BOOT_LDFLAGS='$(list --LD-rp gmp[$gmp_v] mpfr[$mpfr_v] mpc[$mpc_v] isl[$isl_v])' $(get_make_parallel)"
+# make check requires autogen installed
+pack_cmd "make -k check > tmp.test 2>&1 ; echo 'Succes'"
+pack_cmd "make install"
+pack_set_mv_test tmp.test
+
+# Add to LD_LIBRARY_PATH, this ensures that at least 
+# these libraries always will be present in LD
+pack_set --module-opt "--prepend-ENV LD_LIBRARY_PATH=$(pack_get --prefix)/lib64"
---
+++ compiler/gcc/7/isl.bash
@@ -0,0 +1,24 @@
+isl_v=0.16.1
+add_package --build generic \
+	    ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-$isl_v.tar.bz2
+
+pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR
+
+pack_set --module-requirement gmp[$gmp_v]
+
+pack_set --install-query $(pack_get --prefix)/lib/libisl.a
+
+pack_cmd "module load build-tools"
+
+# Install commands that it should run
+pack_cmd "../configure" \
+         "--prefix $(pack_get --prefix)" \
+         "--with-gmp-prefix=$(pack_get --prefix gmp[$gmp_v])"
+
+# Make commands
+pack_cmd "make $(get_make_parallel)"
+pack_cmd "make check > tmp.test 2>&1"
+pack_cmd "make install"
+pack_set_mv_test tmp.test
+
+pack_cmd "module unload build-tools"
