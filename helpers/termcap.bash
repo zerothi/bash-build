6d72ab59f9eda31f43f6f0662d696d98c10b547e
Nick Papior <nickpapior@gmail.com>
2017-02-07 21:25:45 +0100

    fixed termcap library with fPIC
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- helpers/termcap.bash
+++ helpers/termcap.bash
@@ -8,6 +8,9 @@ pack_set --install-query $(pack_get --prefix)/lib/libtermcap.a
 # Install commands that it should run
 pack_cmd "./configure --prefix=$(pack_get --prefix)"
 
+# Fix Makefile
+pack_cmd "sed -i -e 's/^CFLAGS.*/CFLAGS = $CFLAGS/' Makefile"
+
 # Make commands
 pack_cmd "make"
 pack_cmd "make install"
