deb12eb8dd5d73a3678e18d2b747d09cf9cd7a57
Nick Papior <nickpapior@gmail.com>
2017-02-28 08:51:22 +0100

    bug fixed, flook
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/flook.bash
+++ libs/flook.bash
@@ -8,13 +8,16 @@ pack_set -s $IS_MODULE -s $BUILD_DIR
 pack_set --install-query $(pack_get --LD)/libflookall.a
 
 # Compile
-pack_cmd "echo 'TOP_DIR=..' > setup.make"
+pack_cmd "echo '# NRP setup.make' > setup.make"
 pack_cmd "sed -i '$ a\
 CC = $CC\n\
 FC = $FC\n\
 CFLAGS = $CFLAGS\n\
-FCFLAGS = $FCFLAGS\n\
-include ../Makefile' setup.make"
+FCFLAGS = $FCFLAGS\n' setup.make"
+
+# Create makefile
+pack_cmd "echo 'TOP_DIR=..' > Makefile"
+pack_cmd "echo 'include ../Makefile' >> Makefile"
 
 pack_cmd "make liball"
 
