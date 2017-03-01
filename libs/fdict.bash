144a69971ba77db75c07b3e485ef8e85a2a18bb6
Nick Papior <nickpapior@gmail.com>
2017-03-01 13:48:46 +0100

    updated fdict and ncdf
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/fdict.bash
+++ libs/fdict.bash
@@ -1,9 +1,8 @@
-v=0.4.3
+v=0.5.0
 add_package \
-    --archive fdict-$v.tar.gz \
-    https://github.com/zerothi/fdict/archive/$v.tar.gz
+    https://github.com/zerothi/fdict/releases/download/v$v/fdict-$v.tar.gz
 
-pack_set -s $IS_MODULE
+pack_set -s $IS_MODULE -s $BUILD_DIR
 
 pack_set --install-query $(pack_get --prefix)/lib/libfdict.a
 
@@ -12,10 +11,14 @@ file=setup.make
 pack_cmd "echo '# Hello' > $file"
 pack_cmd "sed -i '1 a\
 FC = $FC\n\
-FC_SERIAL = $FC\n\
 FFLAGS = $FCFLAGS\n\
+INCLUDES = -I.\n\
 AR = $AR\n\
+RANLIB = $RANLIB\n\
 ' $file"
+
+pack_cmd "echo 'TOP_DIR=..' > Makefile"
+pack_cmd "echo 'include ../Makefile' >> Makefile"
 
 # Make commands
 pack_cmd "make $(get_make_parallel)"
--- libs/ncdf.bash
+++ libs/ncdf.bash
@@ -1,9 +1,9 @@
-v=0.3.2
+v=0.4.0
 add_package \
     --archive ncdf-$v.tar.gz \
-    https://github.com/zerothi/ncdf/archive/$v.tar.gz
+    https://github.com/zerothi/ncdf/releases/download/v$v/ncdf-$v.tar.gz
 
-pack_set -s $IS_MODULE
+pack_set -s $IS_MODULE -s $BUILD_DIR
 
 pack_set --install-query $(pack_get --prefix)/lib/libncdf.a
 
@@ -13,22 +13,25 @@ pack_set --module-requirement netcdf
 # Create the arch-make file
 file=setup.make
 pack_cmd "echo '# Hello' > $file"
-pack_cmd "sed -i '1 a\
+pack_cmd "sed -i '$ a\
 FC = $MPIFC\n\
 FFLAGS = $FFLAGS\n\
-CPP = cpp -E -P -C \n\
 FDICT_PREFIX = $(pack_get --prefix fdict)\n\
-INCLUDES = $(list --INCDIRS $(pack_get --mod-req-path))\n\
+INCLUDES = $(list --INCDIRS $(pack_get --mod-req-path)) -I../src\n\
 LIB_PATH = $(list --LD-rp $(pack_get --mod-req-path))\n\
 LIBS = \$(LIB_PATH) -lfdict -lnetcdff -lnetcdf -lpnetcdf -lhdf5hl_fortran -lhdf5_fortran -lhdf5_hl -lhdf5 -lz\n\
 FPPFLAGS = -DNCDF_PARALLEL -DNCDF_4\n\
 AR = $AR\n\
+RANLIB = $RANLIB\n\
 ' $file"
 
+pack_cmd "echo 'CDF=4' > Makefile"
+pack_cmd "echo 'MPI=1' >> Makefile"
+pack_cmd "echo 'TOP_DIR=..' >> Makefile"
+pack_cmd "echo 'include ../Makefile' >> Makefile"
+
 # Make commands
-pack_cmd "cp $(pack_get --prefix fdict)/bin/settings.bash ."
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make install PREFIX=$(pack_get --prefix)"
 pack_cmd "make test > tmp.test 2>&1 || echo 'Fail'"
 pack_set_mv_test tmp.test
-
