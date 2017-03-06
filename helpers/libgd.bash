743b7d823f000534d16a1ce37fc45997abd62ec0
Nick Papior <nickpapior@gmail.com>
2017-03-06 14:10:05 +0100

    fixed libgd compilation
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- helpers/libgd.bash
+++ helpers/libgd.bash
@@ -9,6 +9,7 @@ pack_set --install-query $(pack_get --LD)/libgd.a
 pack_cmd "CC=$CC CFLAGS='$CFLAGS' ./configure" \
 	 "--prefix=$(pack_get --prefix)"
 
+pack_cmd "sed -i -e \"/math.h/a#include <limits.h>\n\" src/gd_gd2.c"
 # Make commands
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make install"
