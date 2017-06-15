d6f8baeee78bbd2a7939b61ef654e2c199342367
Nick Papior <nickpapior@gmail.com>
2017-06-15 20:21:26 +0200

    removed GCC-4 because it is out-dated.
    Now the oldest version is 5.4.0
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- compiler/gcc/gcc.bash
+++ compiler/gcc/gcc.bash
@@ -1,13 +1,3 @@
-gcc_v=4.9.4
-gcc=gcc_$gcc_v
-source_pack compiler/gcc/prereq.bash
-source_pack compiler/gcc/4/gmp.bash
-source_pack compiler/gcc/4/mpfr.bash
-source_pack compiler/gcc/4/mpc.bash
-source_pack compiler/gcc/4/isl.bash
-source_pack compiler/gcc/4/cloog.bash
-source_pack compiler/gcc/4/gcc.bash
-
 gcc_v=5.4.0
 gcc=gcc_$gcc_v
 source_pack compiler/gcc/prereq.bash
