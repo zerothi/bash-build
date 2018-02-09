0870561eb03c9729462b8ac7b359787ae69a8955
Nick Papior <nickpapior@gmail.com>
2018-02-09 16:04:22 +0100

    fixed pkgconfig issue by installing it
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ helpers/pkgconfig.bash
@@ -0,0 +1,15 @@
+add_package --build generic https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
+
+pack_set -s $MAKE_PARALLEL -s $BUILD_DIR
+
+pack_set --prefix $(pack_get --prefix build-tools)
+
+pack_set --install-query $(pack_get --prefix)/bin/pkg-config
+
+# Install commands that it should run
+pack_cmd "../configure" \
+	 "--prefix $(pack_get --prefix)"
+
+# Make commands
+pack_cmd "make $(get_make_parallel)"
+pack_cmd "make install"
