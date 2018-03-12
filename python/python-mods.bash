f8dceed0705983514e923066dff66d94c29eb264
Nick Papior <nickpapior@gmail.com>
2018-03-12 21:59:09 +0100

    added inelastica to numerics
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/python-mods.bash
+++ python/python-mods.bash
@@ -71,7 +71,7 @@ rm_latest python$pV.numerics/$(get_c)
 tmp=
 for i in scipy cython mpi4py netcdf4py matplotlib sympy pandas \
 	 h5py numexpr theano numba seaborn networkx \
-	 sisl kwant pybinding phonopy pythtb qutip \
+	 sisl Inelastica kwant pybinding phonopy pythtb qutip \
 	 dask xarray yt \
 	 pyamg scikit-learn scikit-nano scikit-optimize ; do
     if [[ $(pack_installed $i) -eq 1 ]]; then
