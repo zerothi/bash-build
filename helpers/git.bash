2adf988d8ef4028f94811f12070cd32fd1e019db
Nick Papior <nickpapior@gmail.com>
2018-04-09 11:50:37 +0200

    added git LFS to git commands
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- helpers/git.bash
+++ helpers/git.bash
@@ -1,5 +1,5 @@
 # apt-get install gettext
-for v in 2.14.2 ; do
+for v in 2.17.0 ; do
 add_package --build generic \
 	    --archive git-$v.tar.gz \
 	    https://github.com/git/git/archive/v$v.tar.gz
@@ -9,7 +9,7 @@ pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 pack_set --module-requirement gen-zlib
 
 pack_set --module-opt "--lua-family git"
-pack_set --install-query $(pack_get --prefix)/bin/git
+pack_set --install-query $(pack_get --prefix)/bin/git-lfs
 
 # Preload all tools for creating the configure script
 tmp="$(pack_get --mod-req-all build-tools) $(pack_get --module-name build-tools)"
@@ -28,6 +28,14 @@ pack_cmd "make $(get_make_parallel)"
 #pack_cmd "make test > tmp.test 2>&1"
 pack_cmd "make install"
 #pack_set_mv_test tmp.test
+
+# Now install the git-packages...
+o=$(pwd_archives)/$(pack_get --package)-lfs-2.4.0.tar.gz
+dwn_file https://github.com/git-lfs/git-lfs/releases/download/v2.4.0/git-lfs-linux-amd64-2.4.0.tar.gz $o
+pack_cmd "mkdir lfs ; cd lfs"
+pack_cmd "tar xfz $o ; cd git-lfs*"
+pack_cmd "PREFIX=$(pack_get --prefix)  ./install.sh"
+pack_cmd "cd ../../"
 
 done
 
