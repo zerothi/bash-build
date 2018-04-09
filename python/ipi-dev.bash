32742ba8ecbf5e3efd09227d9474ae414d6d8972
Nick Papior <nickpapior@gmail.com>
2018-04-09 14:43:22 +0200

    added ipi-dev
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ python/ipi-dev.bash
@@ -0,0 +1,23 @@
+[ "x${pV:0:1}" == "x3" ] && return 0
+
+add_package --package ipi-dev --version 0 \
+	    https://github.com/cosmo-epfl/i-pi-dev.git
+
+pack_set -s $IS_MODULE -s $PRELOAD_MODULE
+
+pack_set --install-query always-install-this-module
+
+pack_set $(list --prefix ' --module-requirement ' scipy)
+
+pack_cmd "mkdir -p $(pack_get --prefix)/lib/python$pV/site-packages"
+
+pack_cmd "$(get_parent_exec) setup.py install" \
+	 "--prefix=$(pack_get --prefix)"
+
+# env.sh file
+pack_cmd "cp env.sh $(pack_get --prefix)/"
+pack_set --module-opt "--set-ENV IPI_ENV=$(pack_get --prefix)/env.sh"
+
+pack_cmd "mkdir -p $(pack_get --prefix)/source"
+pack_cmd "cd drivers"
+pack_cmd "cp Makefile driver.f90 fsockets.f90 fsockets_pure.f90 sockets.c $(pack_get --prefix)/source/"
