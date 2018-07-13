5350940603b5dc199da1afde265b7fd69043e795
Nick Papior <nickpapior@gmail.com>
2018-07-13 21:41:23 +0200

    fixed meson
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/meson.bash
+++ python/meson.bash
@@ -7,6 +7,8 @@ pack_set --install-query $(pack_get --prefix)/bin/meson
 
 [ "x${pV:0:1}" == "x2" ] && pack_set --host-reject $(get_hostname)
 
+pack_cmd "mkdir -p $(pack_get --LD)/python$pV/site-packages"
+
 # Install commands that it should run
 pack_cmd "$(get_parent_exec) setup.py build"
 pack_cmd "$(get_parent_exec) setup.py install" \
