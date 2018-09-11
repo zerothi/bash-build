960b80923851888b5fbecbdcb1397e48717a02a6
Nick Papior <nickpapior@gmail.com>
2018-09-11 10:52:49 +0200

    fixed modules
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- helpers/modules.bash
+++ helpers/modules.bash
@@ -1,6 +1,7 @@
 # apt-get install tcl8.X-dev
 #add_package --build generic-no-version http://downloads.sourceforge.net/project/modules/Modules/modules-3.2.10/modules-3.2.10.tar.gz
-v=4.1.4
+# 4.1.4 has errors in configure
+v=4.1.0
 add_package --build generic-no-version https://github.com/cea-hpc/modules/releases/download/v$v/modules-$v.tar.bz2
 
 pack_set --install-query $(pack_get --prefix)/$v/bin/envml
