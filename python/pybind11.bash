44537dc0785b2dadc9f8a8f2ee53e2e938fbc07a
Nick Papior <nickpapior@gmail.com>
2018-09-11 10:51:34 +0200

    updated dependency for pybind11
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/pybind11.bash
+++ python/pybind11.bash
@@ -4,6 +4,8 @@ add_package --archive pybind11-$v.tar.gz \
 
 pack_set -s $IS_MODULE -s $BUILD_DIR
 
+pack_set --module-requirement build-tools
+
 pack_set --install-query $(pack_get --prefix)/include/pybind11/pybind11.h
 
 pack_cmd "cmake -DCMAKE_INSTALL_PREFIX=$(pack_get --prefix)" \
