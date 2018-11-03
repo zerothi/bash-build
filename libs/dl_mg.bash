cc027e1fd829cbf72ea9f5f28b7153fbaa7d671d
Nick Papior <nickpapior@gmail.com>
2018-11-03 11:05:23 +0100

    fixed dl_mg
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/dl_mg.bash
+++ libs/dl_mg.bash
@@ -5,6 +5,9 @@ pack_set -s $IS_MODULE
 
 pack_set --install-query $(pack_get --LD)/libdlmg.a
 
+pack_set -lib -ldlmg
+pack_set -lib[omp] -ldlmg_omp
+
 pack_set --module-requirement mpi
 
 file=platforms/nicpa.inc
@@ -19,6 +22,10 @@ USE_OPENMP = yes \n\
 MPIFLAGS = -DMPI \n\
 FFLAGS = $FCFLAGS $FLAG_OMP\n' $file"
 
+pack_cmd "make PLATFORM=nicpa $libdir/libdlmg.a"
+pack_cmd "cp $libdir/libdlmg.a $libdir/libdlmg_omp.a"
+pack_cmd "rm -rf obj *.mod"
+pack_cmd "sed -i 's:$FLAG_OMP::g;s:USE_OPENMP.*:USE_OPENMP = no:g' $file"
 pack_cmd "make PLATFORM=nicpa $libdir/libdlmg.a"
 
 # Ensure we have modules in include directory
