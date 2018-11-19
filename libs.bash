4b7d4524ce32514cac16dbd881263a9c0fa67330
Nick Papior <nickpapior@gmail.com>
2018-11-19 22:19:05 +0100

    updated lots of versions
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs.bash
+++ libs.bash
@@ -32,9 +32,9 @@ source_pack libs/opencoarray.bash
 
 # Default fftw libs
 source_pack libs/fftw2.bash
-source_pack libs/fftw3.bash
+source_pack libs/fftw.bash
 source_pack libs/fftw2-intel.bash
-source_pack libs/fftw3-intel.bash
+source_pack libs/fftw-intel.bash
 source_pack libs/nfft.bash
 
 # ONETEPs multigrid Poisson solver
--- libs/arpack-ng.bash
+++ libs/arpack-ng.bash
@@ -1,4 +1,4 @@
-v=3.4.0
+v=3.6.3
 add_package \
     --archive arpack-ng-$v.tar.gz \
     https://github.com/opencollab/arpack-ng/archive/$v.tar.gz
