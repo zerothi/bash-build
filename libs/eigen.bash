792f6a25dbbf84d647feffad39c763dcd533e01a
Nick Papior <nickpapior@gmail.com>
2018-11-26 09:34:50 +0100

    eigen loads build-tools for cmake
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- libs/eigen.bash
+++ libs/eigen.bash
@@ -9,7 +9,11 @@ pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR
 
 pack_set --install-query $(pack_get --prefix)/include/Eigen/Dense
 
+pack_cmd "module load build-tools"
+
 pack_cmd "cmake ../ -DCMAKE_INSTALL_PREFIX=$(pack_get --prefix)" \
 	 "-DEIGEN_INCLUDE_INSTALL_DIR=$(pack_get --prefix)/include"
 pack_cmd "make install"
+
+pack_cmd "module unload build-tools"
 
