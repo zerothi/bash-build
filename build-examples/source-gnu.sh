63cbf1b04766a27c050cf5289bbd5dd746551d7f
Nick Papior <nickpapior@gmail.com>
2019-01-21 13:41:48 +0100

    fixed build-examples
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- build-examples/source-gnu.sh
+++ build-examples/source-gnu.sh
@@ -3,10 +3,10 @@
 AR=gcc-ar
 RANLIB=gcc-ranlib
 CC=gcc
-CPP="gcc -E -C"
+#CPP="gcc -E"
 CXX=g++
 #CXXCPP="g++ -E"
-FPP="gfortran -cpp -E -C -x none"
+#FPP="gfortran -cpp -E -C -x none"
 F77=gfortran
 F90=gfortran
 FC=gfortran
