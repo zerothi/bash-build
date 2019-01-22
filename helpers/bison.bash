69c2fa83b790c1b045148caa35b76470f867dea6
Nick Papior <nickpapior@gmail.com>
2019-01-22 07:38:10 +0100

    fixed bison compilation
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- helpers/bison.bash
+++ helpers/bison.bash
@@ -1,7 +1,7 @@
 # apt-get m4
 add_package --build generic http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.xz
 
-pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR
+pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR -s $BUILD_TOOLS
 
 pack_set --install-query $(pack_get --prefix)/bin/bison
 
