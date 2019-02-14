478d579527a427e8bbf6d275747ddebe18b08996
Nick Papior <nickpapior@gmail.com>
2019-02-14 15:34:17 +0100

    fixed pybind11 compilation
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/pybind11.bash
+++ python/pybind11.bash
@@ -9,7 +9,7 @@ pack_set --module-requirement eigen
 pack_set --install-query $(pack_get --prefix)/include/pybind11/pybind11.h
 
 pack_cmd "cmake -DCMAKE_INSTALL_PREFIX=$(pack_get --prefix)" \
-	 "-DPYTHON_EXECUTABLE=$(pack_get --prefix python[$pV])/bin/$(get_parent_exec)" ..
+	 "-DPYTHON_EXECUTABLE=$(get_parent_exec) .."
 
 pack_cmd "make $(get_make_parallel)"
 #pack_cmd "make check > pybind11.tmp"
