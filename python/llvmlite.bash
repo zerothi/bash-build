91085d2cb312b08269a1dad21eab3ed14bd80f01
Nick Papior <nickpapior@gmail.com>
2019-04-23 08:52:09 +0200

    fixed llvmlite
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/llvmlite.bash
+++ python/llvmlite.bash
@@ -1,19 +1,19 @@
 # As LLVM is built with gnu-compiler, we should enforce this
 # here as well (this only works with 3.6.0)
 v=0.28.0
-add_package --archive llvmlite-$v.tar.gz \
+add_package -archive llvmlite-$v.tar.gz \
     https://github.com/numba/llvmlite/archive/v$v.tar.gz
 
 pack_set -s $IS_MODULE -s $BUILD_TOOLS
 
-pack_set --install-query $(pack_get --LD)/python$pV/site-packages/llvmlite
+pack_set -install-query $(pack_get -LD)/python$pV/site-packages/llvmlite
 
-pack_set --module-requirement $(get_parent)
-pack_set --module-requirement llvm
+pack_set -module-requirement $(get_parent)
+pack_set -module-requirement llvm[7]
 
-pack_cmd "$(get_parent_exec)" \
-    "setup.py install --prefix=$(pack_get --prefix)"
+pack_cmd "$(get_parent_exec) setup.py install --prefix=$(pack_get -prefix)"
 
-add_test_package llvm-lite.test
+
+add_test_package llvmlite.test
 pack_cmd "$(get_parent_exec) -m llvmlite.tests > $TEST_OUT 2>&1 ; echo 'Success'"
 pack_store $TEST_OUT
--- python/numba.bash
+++ python/numba.bash
@@ -1,20 +1,20 @@
 v=0.43.0
-add_package --archive numba-$v.tar.gz \
+add_package -archive numba-$v.tar.gz \
 	    https://github.com/numba/numba/archive/$v.tar.gz
 
 pack_set -s $IS_MODULE -s $PRELOAD_MODULE
 
-pack_set --module-requirement numpy
-pack_set --module-requirement cython
-pack_set --module-requirement llvmlite
+pack_set -module-requirement numpy
+pack_set -module-requirement cython
+pack_set -module-requirement llvmlite
 
-pack_set --install-query $(pack_get --prefix)/bin/numba
+pack_set -install-query $(pack_get -prefix)/bin/numba
 
-pack_cmd "mkdir -p $(pack_get --prefix)/lib/python$pV/site-packages/"
+pack_cmd "mkdir -p $(pack_get -prefix)/lib/python$pV/site-packages/"
 
-pack_cmd "$(get_parent_exec) setup.py build "
-pack_cmd "$(get_parent_exec) setup.py install" \
-    "--prefix=$(pack_get --prefix)"
+pack_cmd "$(get_parent_exec) setup.py build"
+pack_cmd "$(get_parent_exec) setup.py install --prefix=$(pack_get -prefix)"
+
 
 add_test_package numba.test
 pack_cmd "$(get_parent_exec) -m numba.runtests --exclude-tags='long_running' > $TEST_OUT 2>&1 ; echo 'Success'"
