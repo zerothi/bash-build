6d8e68e0645bc8121bda589f0463b8a2ade5dc7a
Nick Papior <nickpapior@gmail.com>
2019-07-31 23:14:25 +0200

    bumped freetype since harfbuzz requires newer freetype
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- helpers/freetype.bash
+++ helpers/freetype.bash
@@ -1,5 +1,5 @@
 add_package -build generic -alias gen-freetype -package gen-freetype \
-    http://download.savannah.gnu.org/releases/freetype/freetype-2.8.1.tar.bz2
+    http://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.xz
 
 pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 
@@ -8,13 +8,11 @@ pack_set -install-query $(pack_get -prefix)/bin/freetype-config
 # pkg-config will get the correct library
 pack_set -module-requirement gen-libpng
 
-# Configure calls setup
-pack_cmd "./configure --prefix $(pack_get -prefix)"
+pack_cmd "./configure --prefix $(pack_get -prefix) --enable-freetype-config"
 pack_cmd "make"
 pack_cmd "make install"
 
 # Sadly some installations does not check the correct
 # place for the ft2build.h header
 pack_cmd "pushd $(pack_get -prefix)/include ; ln -s freetype2/ft2build.h ; ln -s freetype2/freetype freetype ; popd"
-
 
--- helpers/libpng.bash
+++ helpers/libpng.bash
@@ -1,16 +1,16 @@
-add_package --build generic --alias gen-libpng --package gen-libpng \
+add_package -build generic -alias gen-libpng -package gen-libpng \
 	    https://download.sourceforge.net/libpng/libpng-1.6.36.tar.xz
 
 pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 
-pack_set --install-query $(pack_get --LD)/libpng.a
+pack_set -install-query $(pack_get -LD)/libpng.a
 
-pack_set --module-requirement gen-zlib
+pack_set -module-requirement gen-zlib
 
 # Install commands that it should run
 pack_cmd "./configure" \
-	 "--prefix $(pack_get --prefix)" \
-	 "--with-zlib-prefix=$(pack_get --prefix gen-zlib)"
+	 "--prefix $(pack_get -prefix)" \
+	 "--with-zlib-prefix=$(pack_get -prefix gen-zlib)"
 
 # Make commands
 pack_cmd "make $(get_make_parallel)"
