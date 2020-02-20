fdd43718830d6070363ecdd20acae4bb8e933deb
Nick Papior <nickpapior@gmail.com>
2020-02-20 12:59:32 +0100

    maintenance on luajit (no changes)
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- lua/luajit.bash
+++ lua/luajit.bash
@@ -1,10 +1,10 @@
-add_package --build generic \
-    --package luajit \
+add_package -build generic \
+    -package luajit \
     http://luajit.org/download/LuaJIT-2.0.5.tar.gz
 
-pack_set --module-requirement lua
+pack_set -module-requirement lua
 
-pack_set --install-query $(pack_get --prefix lua)/bin/luajit
+pack_set -install-query $(pack_get -prefix lua)/bin/luajit
 
-pack_cmd "make install PREFIX=$(pack_get --prefix lua) CC='$CC' CFLAGS='$CFLAGS'"
+pack_cmd "make install PREFIX=$(pack_get -prefix lua) CC='$CC' CFLAGS='$CFLAGS'"
 
