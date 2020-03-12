fc93c9fdbc0ec12f5d59f7a95749bead289bb26d
Nick Papior <nickpapior@gmail.com>
2020-03-12 15:29:37 +0100

    fixed rdkit
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/rdkit.bash
+++ python/rdkit.bash
@@ -21,7 +21,9 @@ tmp_flags="$tmp_flags -DBOOST_ROOT=$(pack_get -prefix boost)"
 tmp_flags="$tmp_flags -DBoost_NO_SYSTEM_PATHS=ON"
 tmp_flags="$tmp_flags -DRDK_BUILD_CAIRO_SUPPORT=ON"
 tmp_flags="$tmp_flags -DEIGEN3_INCLUDE_DIR=$(pack_get -prefix eigen)/include"
-tmp_flags="$tmp_flags -DPYTHON_LIBRARY=$(pack_get -LD $(get_parent))/libpython${pV}m.a -DPYTHON_INCLUDE_DIR=$(pack_get -prefix $(get_parent))/include/python${pV}m -DPYTHON_EXECUTABLE=$(get_parent_exec)"
+_p=$(pack_get -prefix $(get_parent))
+[ -e $_p/include/python${pV}m ] && v=${pV}m || v=${pV}
+tmp_flags="$tmp_flags -DPYTHON_LIBRARY=$(pack_get -LD $(get_parent))/libpython$v.a -DPYTHON_INCLUDE_DIR=$_p/include/python$v -DPYTHON_EXECUTABLE=$(get_parent_exec)"
 
 pack_cmd "cmake $tmp_flags .."
 pack_cmd "make $(get_make_parallel)"
