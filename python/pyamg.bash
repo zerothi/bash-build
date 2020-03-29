14aca5124a4217d0276298f50d48ced2b1456da1
Nick Papior <nickpapior@gmail.com>
2020-03-29 20:58:09 +0200

    cleaned pyamg
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- python/pyamg.bash
+++ python/pyamg.bash
@@ -1,20 +1,20 @@
 v=4.0.0
 add_package \
-    --archive pyamg-$v.tar.gz --version $v \
+    -archive pyamg-$v.tar.gz -version $v \
     https://github.com/pyamg/pyamg/archive/v$v.tar.gz
 
 pack_set -s $IS_MODULE -s $PRELOAD_MODULE
 
-pack_set --directory pyamg-$v
+pack_set -directory pyamg-$v
 
-pack_set --install-query $(pack_get --LD)/python$pV/site-packages/site.py
+pack_set -install-query $(pack_get -LD)/python$pV/site-packages/site.py
 
-pack_set --module-requirement scipy
-pack_set --module-requirement pybind11
+pack_set -module-requirement scipy
+pack_set -module-requirement pybind11
 
-pack_cmd "mkdir -p $(pack_get --prefix)/lib/python$pV/site-packages"
+pack_cmd "mkdir -p $(pack_get -prefix)/lib/python$pV/site-packages"
 
 # Install commands that it should run (pyamg does not use numpy builds)
 pack_cmd "$(get_parent_exec) setup.py build"
 pack_cmd "$(get_parent_exec) setup.py install" \
-    "--prefix=$(pack_get --prefix)"
+    "--prefix=$(pack_get -prefix)"
