2a6eebe798bfab6ac5fc867a93ec400188e08974
Nick Papior <nickpapior@gmail.com>
2020-06-30 23:01:30 +0200

    enabled openjpeg in ffmpeg

--- helpers/ffmpeg.bash
+++ helpers/ffmpeg.bash
@@ -6,14 +6,17 @@ pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 
 pack_set -install-query $(pack_get -prefix)/bin/ffmpeg
 
+pack_set -mod-req openjpeg
+
 tmp=
 # This ensures that we can encode x264 files (if header exists)
 [[ -e /usr/include/x264.h ]] && tmp="$tmp --enable-libx264"
 
 # Install commands that it should run
 pack_cmd "./configure $tmp" \
-	 "--prefix=$(pack_get -prefix)" \
-     --enable-shared \
+	 --prefix=$(pack_get -prefix) \
+	 --enable-libopenjpeg \
+	 --enable-shared \
 	 --disable-yasm \
 	 --enable-gpl --enable-libpulse
 
