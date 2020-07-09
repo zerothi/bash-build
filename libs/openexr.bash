eb4c1ffd21261589e9f41cc840dc15bb78eea50c
Nick Papior <nickpapior@gmail.com>
2020-07-09 11:20:26 +0200

    added openexr

---
+++ libs/openexr.bash
@@ -0,0 +1,21 @@
+v=2.5.1
+add_package -archive openexr-$v.tar.gz \
+	    https://github.com/AcademySoftwareFoundation/openexr/archive/v$v.tar.gz
+
+pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR
+pack_set -build-mod-req build-tools
+
+pack_set -mod-req boost -mod-req zlib
+
+pack_set -install-query $(pack_get -LD)/libIex.so
+
+pack_cmd "cmake -DCMAKE_INSTALL_PREFIX=$(pack_get -prefix)" \
+	 -DBOOST_ROOT=$(pack_get -prefix boost) \
+	 -DPYILMBASE_ENABLE=0 \
+	 ..
+
+pack_cmd "make $(get_make_parallel)"
+pack_cmd "make install"
+
+
+
