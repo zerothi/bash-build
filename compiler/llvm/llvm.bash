5c53f7fefb6ab7cf51e7097681bef46218e88402
Nick Papior <nickpapior@gmail.com>
2020-08-06 10:30:39 +0200

    upgrade julia and allowed llvm compilation

--- compiler/llvm/llvm.bash
+++ compiler/llvm/llvm.bash
@@ -1,4 +1,4 @@
 source_pack compiler/llvm/7/llvm.bash
-#source_pack compiler/llvm/8/llvm.bash
+source_pack compiler/llvm/8/llvm.bash
 source_pack compiler/llvm/9/llvm.bash
 source_pack compiler/llvm/10/llvm.bash
--- helpers/libffi.bash
+++ helpers/libffi.bash
@@ -1,13 +1,12 @@
-add_package --build generic --alias gen-libffi --package gen-libffi \
+add_package -build generic -alias gen-libffi -package gen-libffi \
 	    ftp://sourceware.org/pub/libffi/libffi-3.3.tar.gz
 
 pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 
-pack_set --install-query $(pack_get --prefix)/include/ffi.h
+pack_set -install-query $(pack_get -prefix)/include/ffi.h
 
 # Install commands that it should run
-pack_cmd "./configure" \
-	 "--prefix $(pack_get --prefix)"
+pack_cmd "./configure --prefix $(pack_get -prefix)"
 
 # Make commands
 pack_cmd "make $(get_make_parallel)"
@@ -21,10 +20,10 @@ pack_cmd "make install"
 
 # Fix include path and pkgconfig
 for f in lib lib64 ; do
-    flib="$(pack_get --prefix)/$f/pkgconfig/libffi.pc"
+    flib="$(pack_get -prefix)/$f/pkgconfig/libffi.pc"
     pack_cmd "[ -e $flib ] && sed -i -e 's:includedir=.*:includedir=\${prefix}/include:' $flib || true"
-    flib="$(pack_get --prefix)/$f/libffi-$(pack_get --version)"
-    pack_cmd "[ -d $flib/include ] && mv $flib/include $(pack_get --prefix)/include || true"
+    flib="$(pack_get -prefix)/$f/libffi-$(pack_get -version)"
+    pack_cmd "[ -d $flib/include ] && mv $flib/include $(pack_get -prefix)/include || true"
     pack_cmd "[ -d $flib ] && rm -rf $flib || true"
 done
 unset flib
