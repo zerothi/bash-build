47dbba1fe16eff84f62bb42d393eadf32b204ff3
Nick Papior <nickpapior@gmail.com>
2020-09-09 13:02:02 +0200

    reverted setuptools for py2

--- python/pip.bash
+++ python/pip.bash
@@ -1,9 +1,8 @@
 v=20.1.1
-add_package --archive pip-$v.tar.gz \
+add_package -archive pip-$v.tar.gz \
      https://github.com/pypa/pip/archive/$v.tar.gz
 
-pack_set --module-requirement $(get_parent)
-pack_set --install-query $(pack_get --prefix $(get_parent))/bin/pip
+pack_set -module-requirement $(get_parent)
+pack_set -install-query $(pack_get -prefix $(get_parent))/bin/pip
 
-pack_cmd "$(get_parent_exec) setup.py install" \
-    "--prefix=$(pack_get --prefix $(get_parent))"
+pack_cmd "$(get_parent_exec) setup.py install --prefix=$(pack_get -prefix $(get_parent))"
--- python/setuptools.bash
+++ python/setuptools.bash
@@ -1,11 +1,15 @@
-v=49.1.0
-add_package --archive setuptools-$v.tar.gz \
+if [[ "x${pV:0:1}" == "x3" ]]; then
+    v=49.1.0
+else
+    v=40.5.0
+fi
+	
+add_package -archive setuptools-$v.tar.gz \
 	    https://github.com/pypa/setuptools/archive/v$v.tar.gz
 
-pack_set --install-query $(pack_get --prefix $(get_parent))/lib/python$pV/site-packages/setuptools.pth
+pack_set -install-query $(pack_get -prefix $(get_parent))/lib/python$pV/site-packages/setuptools.pth
 
 pack_cmd "$(get_parent_exec) bootstrap.py"
-pack_cmd "$(get_parent_exec) setup.py install" \
-    "--prefix=$(pack_get --prefix $(get_parent))"
+pack_cmd "$(get_parent_exec) setup.py install --prefix=$(pack_get -prefix $(get_parent))"
 
 
