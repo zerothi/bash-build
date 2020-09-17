f085825f2db03fa30fd28fe25256d08b35233ad8
Nick Papior <nickpapior@gmail.com>
2020-09-17 10:55:00 +0200

    added tinker

---
+++ applications/tinker.bash
@@ -0,0 +1,29 @@
+v=8.8.1
+add_package -directory tinker \
+	    https://dasher.wustl.edu/tinker/downloads/tinker-$v.tar.gz
+
+pack_set -s $MAKE_PARALLEL
+
+pack_set -install-query $(pack_get -prefix)/bin/tinker
+
+pack_set -mod-req fftw
+
+# Install commands that it should run
+pack_cmd "mkdir -p $(pack_get -prefix)/bin"
+pack_cmd "cd source"
+pack_cmd "cp ../make/Makefile ."
+
+# append stuff
+pack_cmd "sed -i '$ a\
+TINKERDIR = $(pack_get -prefix)\n\
+LINKDIR = $(pack_get -prefix)/bin\n\
+FFTWDIR = $(pack_get -prefix fftw)\n\
+F77 = $FC \n\
+F77FLAGS = -c $FCFLAGS \n\
+OPTFLAGS = $FCFLAGS\n\
+RENAME = rename_bin\n\
+RANLIB = $RANLIB -c\n\
+LINKFLAGS = \$(OPTFLAGS)\n' Makefile"
+
+pack_cmd "make all $(get_make_parallel)"
+pack_cmd "make install"
