c9e21a4de21f1ec5681a377f13a1845a589ef05e
Nick Papior <nickpapior@gmail.com>
2020-10-13 15:18:20 +0200

    streamlined hdf5 link

--- libs/hdf5.bash
+++ libs/hdf5.bash
@@ -3,7 +3,7 @@ for p in 1.12.0 ; do
 
 add_package \
     --package hdf5 \
-    https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-$p/src/hdf5-$p.tar.bz2
+    https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${p%.*}/hdf5-$p/src/hdf5-$p.tar.bz2
 
 pack_set -s $BUILD_DIR -s $MAKE_PARALLEL -s $IS_MODULE
 
