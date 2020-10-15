e0cd4ee464a8da8f54e6a058e4dd03bddec470ce
Nick Papior <nickpapior@gmail.com>
2020-10-15 11:19:03 +0200

    fixed openmm with external doxygen

--- helpers/doxygen.bash
+++ helpers/doxygen.bash
@@ -1,17 +1,16 @@
 v=1.8.18
-add_package --build generic \
-	    --version $v --package doxygen \
-	    --archive doxygen-Release_${v//./_}.tar.gz \
+add_package -build generic \
+	    -version $v -package doxygen \
+	    -archive doxygen-Release_${v//./_}.tar.gz \
 	    https://github.com/doxygen/doxygen/archive/Release_${v//./_}.tar.gz
 
 pack_set -s $MAKE_PARALLEL -s $IS_MODULE -s $BUILD_DIR
 
 pack_set -build-mod-req build-tools
-pack_set --install-query $(pack_get --prefix)/bin/doxygen
+pack_set -install-query $(pack_get -prefix)/bin/doxygen
 
 pack_cmd "cmake -G 'Unix Makefiles'" \
-	 "-D use-libclang=ON" \
-	 "-D CMAKE_INSTALL_PREFIX=$(pack_get --prefix) ../"
+	 "-D CMAKE_INSTALL_PREFIX=$(pack_get -prefix) ../"
 
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make install"
