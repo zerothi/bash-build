276a240530135b52510bb7898958ea721e391a42
Nick Papior <nickpapior@gmail.com>
2022-01-06 08:26:30 +0100

    mnt: added pythran

---
+++ python/pythran.bash
@@ -0,0 +1,12 @@
+v=0.11.0
+add_package -archive pythran-$v.tar.gz \
+	    https://github.com/serge-sans-paille/pythran/archive/refs/tags/$v.tar.gz
+
+pack_set -s $IS_MODULE -s $PRELOAD_MODULE
+
+pack_set -install-query $(pack_get -LD)/python$pV/site-packages/pythran
+
+pack_set $(list -prefix ' -module-requirement ' numpy)
+
+pack_cmd "mkdir -p $(pack_get --prefix)/lib/python$pV/site-packages"
+pack_cmd "$_pip_cmd . --prefix=$(pack_get -prefix)"
