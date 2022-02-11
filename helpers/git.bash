0555ed07fff4a96cc54d922c0e4798b79f189a3b
Nick Papior <nickpapior@gmail.com>
2022-02-11 08:24:52 +0100

    updated git

--- helpers/git.bash
+++ helpers/git.bash
@@ -1,5 +1,5 @@
 # apt-get install gettext libcurl4-openssl-dev
-for v in 2.26.0 ; do
+for v in 2.26.0 2.35.1 ; do
 add_package -build generic \
 	    -archive git-$v.tar.gz \
 	    https://github.com/git/git/archive/v$v.tar.gz
