da75c748155f1d61a2ac89fc3579feb48e7cf060
Nick Papior <nickpapior@gmail.com>
2023-02-03 14:13:35 +0100

    mnt: fixed siesta git builds

--- applications/siesta-git.bash
+++ applications/siesta-git.bash
@@ -1,4 +1,4 @@
-pack_set -s $MAKE_PARALLEL
+#pack_set -s $MAKE_PARALLEL
 
 # Add the lua family
 pack_set -module-opt "--lua-family siesta"
@@ -20,16 +20,39 @@ pack_cmd 'sed -i -e "s:__FILE__:Fstack.T90:g" Src/Fstack.T90'
 # override version.info
 pack_cmd "git describe --always > version.info"
 
+# Fix SpPivot
+pack_cmd 'sed -i -e "s/\(-o pvtsp \$(OBJS)\)/\1 \$(LIBS)/g" Util/SpPivot/Makefile'
+# Remove vnl-operator compilation
+pack_cmd "sed -i -e '/Pseudo\/vnl-operator/d' Makefile"
+
+Pseudo/vnl-operator
+
 # Change to directory:
-pack_cmd "cd Obj"
+pack_cmd "mkdir -p Obj ; cd Obj"
 
 # Setup the compilation scheme
-pack_cmd "../Src/obj_setup.sh"
+pack_cmd "../Config/obj_setup.sh"
+# remove the LIBS= line
+pack_cmd "sed -i -e '/^LIBS=\s*$/d' build.mk"
+pack_cmd "sed -i -e '/^FPPFLAGS=\s*$/d' build.mk"
 
 prefix=$(pack_get -prefix)
 
 pack_cmd "echo '# Compilation $(pack_get -version) on $(get_c)' > arch.make"
-pack_cmd "echo 'PP = cpp -E -P -C -nostdinc' > arch.make"
+
+pack_cmd "sed -i '$ a\
+SIESTA_INSTALL_DIRECTORY = $prefix\n\
+PP = cpp -E -P -C -nostdinc\n\
+WITH_PSML = 1\n\
+WITH_LIBXC = 1\n\
+WITH_GRIDXC = 1\n\
+WITH_MPI = 1\n\
+WITH_ELPA = 1\n\
+WITH_FLOOK = 1\n\
+WITH_NETCDF = 1\n\
+WITH_NCDF = 1\n\
+WITH_NCDF_PARALLEL = 1\n\
+WITH_EXTRA_FPPFLAGS += TS_NOCHECKS\n' arch.make"
 
 # Add LTO in case of gcc-6.1 and above version 4.1
 # We need to fix LTO compilation for MatrixSwitch library
@@ -45,26 +68,24 @@ pack_cmd "echo 'PP = cpp -E -P -C -nostdinc' > arch.make"
 
 pack_set -module-requirement fftw
 pack_cmd "sed -i '$ a\
-FFTW_PATH = $prefix\n\
-FFTW_INCFLAGS = -I\$(FFTW_PATH)/include\n\
-FFTW_LIBS = -L\$(FFTW_PATH)/lib -lfftw3\n\
-FPPFLAGS += -DNCDF -DNCDF_4 -DNCDF_PARALLEL -DTS_NOCHECKS\n\
-COMP_LIBS += libncdf.a libfdict.a' arch.make"
+FFTW_ROOT = $(pack_get -prefix fftw)\n\
+FFTW_INCFLAGS = -I\$(FFTW_ROOT)/include\n\
+FFTW_LIBS = -L\$(FFTW_ROOT)/lib -lfftw3\n' arch.make"
 
 if [[ $(pack_installed mumps) -eq 1 ]]; then
     pack_set -module-requirement mumps
     pack_cmd "sed -i '$ a\
-METIS_LIB = -lmetis\n\
+METIS_LIB = $(list -LD-rp metis) -lmetis\n\
 LIBS += \$(METIS_LIB)\n\
-FPPFLAGS += -DSIESTA__METIS -DSIESTA__MUMPS\n\
-ADDLIB += -lzmumps -lmumps_common -lesmumps -lscotch -lscotcherr -lpord -lparmetis -lmetis' arch.make"
+WITH_EXTRA_FPPFLAGS += SIESTA__METIS SIESTA__MUMPS\n\
+LIBS += $(list -LD-rp mumps) -lzmumps -lmumps_common -lesmumps -lscotch -lscotcherr -lpord -lparmetis -lmetis' arch.make"
 
 elif [[ $(pack_installed metis) -eq 1 ]]; then
     pack_set -module-requirement metis
     pack_cmd "sed -i '$ a\
-METIS_LIB = -lmetis\n\
+METIS_LIB = $(list -LD-rp metis) -lmetis\n\
 LIBS += \$(METIS_LIB)\n\
-FPPFLAGS += -DSIESTA__METIS' arch.make"
+WITH_EXTRA_FPPFLAGS += SIESTA__METIS' arch.make"
 fi
 
 pack_set -module-requirement libgridxc
@@ -74,10 +95,8 @@ pack_cmd "sed -i '$ a\
 LIBXC_ROOT = $(pack_get -prefix libxc)\n\
 XMLF90_ROOT = $(pack_get -prefix xmlf90)\n\
 GRIDXC_ROOT = $(pack_get -prefix libgridxc)\n\
+GRIDXC_CONFIG_PREFIX = dp\n\
 PSML_ROOT = $(pack_get -prefix libpsml)\n\
--include \$(XMLF90_ROOT)/share/org.siesta-project/xmlf90.mk\n\
--include \$(PSML_ROOT)/share/org.siesta-project/psml.mk\n\
--include \$(GRIDXC_ROOT)/share/org.siesta-project/gridxc_dp_mpi.mk\n\
 LIBS += -Wl,-rpath=$(pack_get -LD libxc)\n\
 LIBS += -Wl,-rpath=$(pack_get -LD xmlf90)\n\
 LIBS += -Wl,-rpath=$(pack_get -LD libpsml)\n\
@@ -92,50 +111,44 @@ SIESTA_ARCH=x86_64-linux-$(get_hostname)\n\
 FPP=$MPIFC\n\
 FPP_OUTPUT= \n\
 CC=$CC\n\
-FC=$MPIFC\n\
+FC_PARALLEL=$MPIFC\n\
 FC_SERIAL=$FC\n\
+#FC=$MPIFC\n\
 AR=$AR\n\
 RANLIB=$RANLIB\n\
-SYS=nag\n\
-SP_KIND=4\n\
-DP_KIND=8\n\
-KINDS=\$(SP_KIND) \$(DP_KIND)\n\
 \n\
-FFLAGS=$FCFLAGS\n\
+FFLAGS = $FFLAGS\n\
 FFLAGS += #OMPPLACEHOLDER\n\
-FPPFLAGS += -DMPI -DFC_HAVE_FLUSH -DFC_HAVE_ABORT -DCDF -DCDF4\n\
 \n\
 ARFLAGS_EXTRA=\n\
 \n\
+WITH_EXPLICIT_NETCDF_SYMBOLS = 1\n\
 NETCDF_INCFLAGS=$(list -INCDIRS netcdf)\n\
-NETCDF_LIBS=$(list -LD-rp netcdf)\n\
-ADDLIB=-lnetcdff -lnetcdf -lpnetcdf -lhdf5_hl -lhdf5 -lz\n\
-ADDLIB += #OMPPLACEHOLDER\n\
-INCFLAGS = $(list -INCDIRS $(pack_get -mod-req))\n\
-\n\
-MPI_INTERFACE=libmpi_f90.a\n\
-MPI_INCLUDE=.\n\
-\n\
-' arch.make"
+NETCDF_LIBS = $(list -LD-rp netcdf)\n\
+NETCDF_LIBS +=-lnetcdff -lnetcdf -lpnetcdf -lhdf5_hl -lhdf5 -lz\n\
+LIBS += #OMPPLACEHOLDER\n\
+INCFLAGS += $(list -INCDIRS $(pack_get -mod-req))\n\
+\n' arch.make"
 
 if [[ $(pack_installed flook) -eq 1 ]]; then
     pack_cmd "sed -i '$ a\
-FPPFLAGS += -DSIESTA__FLOOK \n\
-FLOOK_LIB = $(list -LD-rp flook) -lflookall -ldl\n\
-INCFLAGS += $(list -INCDIRS flook)\n\
-LIBS += \$(FLOOK_LIB) \n' arch.make"
+WITH_FLOOK = 1\n\
+FLOOK_ROOT = $(pack_get -prefix flook)\n\
+LIBS += $(list -LD-rp flook)\n' arch.make"
 fi
 
 # ELPA should be added before the linear algebra libraries
 pack_set -module-requirement elpa
 pack_cmd "sed -i '$ a\
-FPPFLAGS += -DSIESTA__ELPA \n\
-ELPA_LIB = $(list -LD-rp elpa) $(pack_get -lib elpa)\n\
-INCFLAGS += $(list -INCDIRS elpa)/elpa\n\
-LIBS += \$(ELPA_LIB) \n' arch.make"
+WITH_ELPA = 1\n\
+ELPA_ROOT = $(pack_get -prefix elpa)\n\
+ELPA_INCLUDE_DIRECTORY = \$(ELPA_ROOT)/include/elpa\n\
+INCFLAGS += $(list -INCDIRS elpa)\n\
+LIBS += $(list -LD-rp elpa) \n' arch.make"
 
 source applications/siesta-linalg.bash
-
+pack_cmd "sed -i '$ a\
+LDFLAGS += \$(LIBS)\n' arch.make"
 
 function set_flag {
     local v=$1 ; shift
@@ -179,19 +192,58 @@ function set_flag {
 }
 
 
-pack_cmd "mkdir -p $prefix/bin"
-
 # Save the arch.make file
 pack_cmd "cp arch.make $prefix/arch.make"
 
-# Shorthand for compiling utilities
-function make_files {
-    while [ $# -gt 0 ]; do
-	local v=$1 ; shift
-	pack_cmd "make $v"
-	pack_cmd "cp $v $prefix/bin/"
+# Compile the 3m equivalent versions, if applicable
+case $siesta_la in
+    mkl|openblas)
+	tmp=1
+	;;
+    *)
+	tmp=0
+	;;
+esac
+
+pack_cmd "make install_utils"
+pack_cmd "cp ../Util/TS/tselecs.sh $prefix/bin/"
+
+
+if [[ $tmp -eq 1 ]]; then
+    # Go back
+    pack_cmd "echo '' >> arch.make ; echo 'FPPFLAGS += -DUSE_GEMM3M' >> arch.make"
+    for omp in openmp none ; do
+
+	set_flag $omp
+	end=${end}_3m
+
+	# Ensure it is clean...
+	pack_cmd "make clean"
+    
+	# This should ensure a correct handling of the version info...
+	pack_cmd "make $(get_make_parallel) install_siesta"
+	[ -n "$end" ] && pack_cmd "mv $prefix/bin/siesta $prefix/bin/siesta$end"
+
+	pack_cmd "echo '#!/bin/sh' > $prefix/bin/transiesta$end"
+	pack_cmd "echo '$prefix/bin/siesta$end --electrode \$@' >> $prefix/bin/transiesta$end"
+
+	pack_cmd "make Util/TS/TBtrans MODE=clean"
+
+	pack_cmd "make Util/TS/TBtrans $(get_make_parallel)"
+	pack_cmd "cp Util/TS/TBtrans/tbtrans $prefix/bin/tbtrans$end"
+
+	pack_cmd "make Util/TS/TBtrans MODE=clean"
+
+	pack_cmd "make Util/TS/TBtrans $(get_make_parallel) MODE=phtrans"
+	pack_cmd "cp Util/TS/TBtrans/phtrans $prefix/bin/phtrans$end"
+
     done
-}
+fi
+
+
+# remove setting again
+pack_cmd "sed -i -e '/USE_GEMM3M/d' arch.make"
+
 
 # We split this into two segments... One with
 # the old way, and one with the new utilities etc.
@@ -203,128 +255,24 @@ for omp in openmp none ; do
     pack_cmd "make clean"
     
     # This should ensure a correct handling of the version info...
-    pack_cmd "make $(get_make_parallel) siesta ; make siesta"
-    pack_cmd "cp siesta $prefix/bin/siesta$end"
+    pack_cmd "make $(get_make_parallel) install_siesta"
+    [ -n "$end" ] && pack_cmd "mv $prefix/bin/siesta $prefix/bin/siesta$end"
 
     pack_cmd "echo '#!/bin/sh' > $prefix/bin/transiesta$end"
     pack_cmd "echo '$prefix/bin/siesta$end --electrode \$@' >> $prefix/bin/transiesta$end"
-    pack_cmd "chmod a+x $prefix/bin/transiesta$end"
+    
+
+    pack_cmd "make Util/TS/TBtrans MODE=clean"
+   
+    pack_cmd "make Util/TS/TBtrans $(get_make_parallel)"
+    [ -n "$end" ] && pack_cmd "cp Util/TS/TBtrans/tbtrans $prefix/bin/tbtrans$end"
+
+    pack_cmd "make Util/TS/TBtrans MODE=clean"
+
+    pack_cmd "make Util/TS/TBtrans $(get_make_parallel) MODE=phtrans"
+    [ -n "$end" ] && pack_cmd "cp Util/TS/TBtrans/phtrans $prefix/bin/phtrans$end"
     
 done
-
-pack_cmd "cd ../Util/Bands"
-make_files gnubands eigfat2plot fat.gplot gnubands
-
-pack_cmd "cd ../Contrib/APostnikov"
-pack_cmd "make all"
-pack_cmd "cp *xsf fmpdos $prefix/bin/"
-
-pack_cmd "cd ../../COOP"
-make_files mprop fat
-
-pack_cmd "cd ../Denchar/Src"
-make_files denchar
-
-pack_cmd "cd ../../Eig2DOS"
-make_files Eig2DOS
-
-pack_cmd "cd ../Gen-basis"
-make_files gen-basis ioncat
-
-pack_cmd "cd ../Grid"
-make_files grid2val grid2cube grid_rotate grid_supercell
-
-pack_cmd "cd ../Grimme/"
-make_files fdf2grimme
-
-pack_cmd "cd ../HSX"
-make_files hs2hsx hsx2hs
-
-pack_cmd "cd ../Optimizer"
-make_files swarm simplex
-
-pack_cmd "cd ../Projections"
-make_files orbmol_proj
-
-pack_cmd "cd ../SpPivot"
-make_files pvtsp
-
-pack_cmd "cd ../STM/simple-stm"
-make_files plstm
-pack_cmd "cd ../ol-stm/Src"
-make_files stm
-pack_cmd "cd ../../"
-
-# Install TS sub-directory...
-pack_cmd "cd ../TS/"
-pack_cmd "cp tselecs.sh $prefix/bin/"
-
-pack_cmd "cd ts2ts"
-make_files ts2ts
-
-pack_cmd "cd ../tshs2tshs/"
-make_files tshs2tshs
-
-pack_cmd "cd ../TBtrans/"
-for omp in openmp none ; do
-    
-    pack_cmd "pushd ../../../Obj"
-    set_flag $omp
-    pack_cmd "popd ; make clean"
-    pack_cmd "make $(get_make_parallel)"
-    pack_cmd "make" # corrects version 
-    pack_cmd "cp tbtrans $prefix/bin/tbtrans$end"
-    pack_cmd "make clean-tbt ; make $(get_make_parallel) phtrans"
-    pack_cmd "make phtrans" # corrects version 
-    pack_cmd "cp phtrans $prefix/bin/phtrans$end"
-    pack_cmd "make clean"
-    
-done
-pack_cmd "cd ../"
-
-# end TS
-
-pack_cmd "cd ../VCA"
-make_files mixps fractional
-
-pack_cmd "cd ../Vibra/Src"
-make_files fcbuild vibra
-
-pack_cmd "cd ../../WFS"
-make_files readwf readwfx wfs2wfsx wfsx2wfs
-
-
-# Compile the 3m equivalent versions, if applicable
-pack_cmd "cd ../../Obj"
-case $siesta_la in
-    mkl|openblas)
-	tmp=1
-	;;
-    *)
-	tmp=0
-	;;
-esac
-
-if [[ $tmp -eq 1 ]]; then
-    # Go back
-    pack_cmd "echo '' >> arch.make ; echo 'FPPFLAGS += -DUSE_GEMM3M' >> arch.make"
-    for omp in openmp none ; do
-
-	set_flag $omp
-	pack_cmd "echo '#!/bin/sh' > $prefix/bin/transiesta${end}_3m"
-	pack_cmd "echo '$prefix/bin/siesta$end --electrode \$@' >> $prefix/bin/transiesta${end}_3m"
-	pack_cmd "chmod a+x $prefix/bin/transiesta${end}_3m"
-	
-	pack_cmd "pushd ../Util/TS/TBtrans ; make clean"
-	pack_cmd "make $(get_make_parallel) ; make"
-	pack_cmd "cp tbtrans $prefix/bin/tbtrans${end}_3m"
-	pack_cmd "make clean-tbt ; make $(get_make_parallel) phtrans ; make phtrans"
-	pack_cmd "cp phtrans $prefix/bin/phtrans${end}_3m"
-	pack_cmd "make clean"
-	pack_cmd "popd"
-
-    done
-fi
 
 unset set_flag
 unset make_files
--- applications/siesta-linalg.bash
+++ applications/siesta-linalg.bash
@@ -2,70 +2,51 @@
 # libraries to siesta
 
 # SIESTA also enables ELPA kernel
-pack_set --module-requirement elpa
+pack_set -module-requirement elpa
 
 # Check for Intel MKL or not
 siesta_la=mkl
 if $(is_c intel) ; then
 
-    pack_cmd "sed -i '1 a\
-LDFLAGS=$MKL_LIB $(list --LD-rp $(pack_get --mod-req-path))\n\
-FPPFLAGS=$(list --INCDIRS $(pack_get --mod-req-path)) -DSIESTA__ELPA\n\
-\n\
-' arch.make"
+#    pack_cmd "sed -i '$ a\
+#LIBS +=$MKL_LIB $(list -LD-rp $(pack_get -mod-req-path))\n' arch.make"
     case $_mpi_version in
 	openmpi)
-	    pack_cmd "sed -i '1 a\
-LIBS=\$(ADDLIB) -lelpa -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -lmkl_lapack95_lp64 -lmkl_blas95_lp64\n\
-LIBS+= -lmkl_intel_lp64 -lmkl_core -lmkl_sequential\n\
-' arch.make"
+	    pack_cmd "sed -i '$ a\
+SCALAPACK_LIBS = $MKL_LIB -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64\n\
+LAPACK_LIBS = $MKL_LIB -lmkl_lapack95_lp64 -lmkl_blas95_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential\n' arch.make"
 	    ;;
 	mpich)
-	    pack_cmd "sed -i '1 a\
-LIBS=\$(ADDLIB) -lelpa -lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -lmkl_lapack95_lp64 -lmkl_blas95_lp64\n\
-LIBS+= -lmkl_intel_lp64 -lmkl_core -lmkl_sequential\n\
-' arch.make"
+	    pack_cmd "sed -i '$ a\
+SCALAPACK_LIBS = $MKL_LIB -lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64\n\
+LAPACK_LIBS = $MKL_LIB -lmkl_lapack95_lp64 -lmkl_blas95_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential\n' arch.make"
 	    ;;
     esac
+    pack_cmd "sed -i '$ a\
+	    LIBS += \$(SCALAPACK_LIBS) \$(LAPACK_LIBS)\n' arch.make"
 
 # If one ever needs MKL with gnu compiler it should be -lmkl_gf_lp64 instead of -lmkl_intel_lp64
 
 elif $(is_c gnu) ; then
     
-    pack_set --module-requirement scalapack
+    pack_set -module-requirement scalapack
     siesta_la=$(pack_choice -i linalg)
     la=lapack-$siesta_la
-    pack_set --module-requirement $la
-    pack_cmd "sed -i '1 a\
-LDFLAGS=$(list --LD-rp $(pack_get --mod-req-path))\n\
-FPPFLAGS=$(list --INCDIRS $(pack_get --mod-req-path)) -DSIESTA__ELPA\n\
-\n\
-BLAS_LIBS= $(pack_get --lib $la) \n\
-LIBS=\$(ADDLIB) -lelpa -lscalapack \$(BLAS_LIBS)\n' arch.make"
+    pack_set -module-requirement $la
+    pack_cmd "sed -i '$ a\
+LIBS += $(list -LD-rp $(pack_get -mod-req-path))\n\
+SCALAPACK_LIBS = -lscalapack\n\
+LAPACK_LIBS = $(pack_get -lib $la)\n\
+LIBS += \$(SCALAPACK_LIBS) \$(LAPACK_LIBS)\n' arch.make"
 
 else
-    doerr "$(pack_get --package)" "Could not recognize the compiler: $(get_c)"
+    doerr "$(pack_get -package)" "Could not recognize the compiler: $(get_c)"
 
 fi
 
 # Add the MRRR libraries (we know they always have MRRR)
 pack_cmd "sed -i '$ a\
-FPPFLAGS += -DSIESTA__MRRR -I$(pack_get --prefix elpa)/include/elpa \n\
-\n\
-' arch.make"
-
-pack_cmd "sed -i '$ a\
-.F.o:\n\
-\t\$(FC) -c \$(FFLAGS) \$(INCFLAGS) \$(FPPFLAGS) \$< \n\
-.F90.o:\n\
-\t\$(FC) -c \$(FFLAGS) \$(INCFLAGS) \$(FPPFLAGS) \$< \n\
-.f.o:\n\
-\t\$(FC) -c \$(FFLAGS) \$(INCFLAGS) \$<\n\
-.c.o:\n\
-\t\$(CC) -c \$(CFLAGS) \$(INCFLAGS) \$(FPPFLAGS) \$<\n\
-.f90.o:\n\
-\t\$(FC) -c \$(FFLAGS) \$(INCFLAGS) \$<\n\
-\n' arch.make"
+WITH_EXTRA_FPPFLAGS += SIESTA__MRRR\n' arch.make"
 
 pack_cmd "[ -e ../Src/atom.F ] && sed -i '$ a\
 atom.o: atom.F\n\
