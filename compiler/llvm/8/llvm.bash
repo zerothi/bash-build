d1151d7a1852dc5bb23b19866a62d5502e76ec25
Nick Papior <nickpapior@gmail.com>
2023-04-28 13:46:12 +0200

    ensured gcc is a dependency of llvm
    This is required since subsequent compilation with llvm may
    require a corresponding libstdc++

--- compiler/llvm/8/llvm.bash
+++ compiler/llvm/8/llvm.bash
@@ -6,7 +6,7 @@ pack_set -s $IS_MODULE -s $BUILD_DIR -s $MAKE_PARALLEL -s $CRT_DEF_MODULE
 pack_set -install-query $(pack_get -prefix)/bin/llvm-ar
 
 pack_set $(list -p '-build-mod-req ' build-tools)
-pack_set $(list -p '-mod-req ' gen-zlib gen-libxml2 gen-libffi unwind gcc)
+pack_set $(list -p '-mod-req ' gcc[$gnu_v] gen-zlib gen-libxml2 gen-libffi unwind)
 
 # Fetch the c-lang to build it along side
 tmp=$(pack_get -url)
--- compiler/llvm/llvm.bash
+++ compiler/llvm/llvm.bash
@@ -1,10 +1,14 @@
 old_ulimit_n=$(ulimit -n)
 ulimit -n 32768
 
+
+gnu_v=9
 #source_pack compiler/llvm/7/llvm.bash
 #source_pack compiler/llvm/8/llvm.bash
 #source_pack compiler/llvm/9/llvm.bash
 source_pack compiler/llvm/10/llvm.bash
+
+gnu_v=12
 source_pack compiler/llvm/11/llvm.bash
 #source_pack compiler/llvm/12/llvm.bash
 #source_pack compiler/llvm/13/llvm.bash
