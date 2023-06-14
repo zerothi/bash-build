09f23780ce6ac2bb6b22cf2e1d916ef7ae2c1fce
Nick Papior <nickpapior@gmail.com>
2023-06-14 12:51:17 +0200

    updated sip source repo

--- python/sip.bash
+++ python/sip.bash
@@ -1,30 +1,19 @@
-v=6.7.7
-add_package https://www.riverbankcomputing.com/static/Downloads/sip/$v/sip-$v.tar.gz
+add_package https://files.pythonhosted.org/packages/48/75/98987181e897ef378d6c239ee733328a7264a41f2d8263e61d7b7c4c0909/sip-6.7.9.tar.gz
 pack_set -s $IS_MODULE
 
-pack_set --install-query $(pack_get --prefix)/bin/sip
+pack_set --install-query $(pack_get -prefix)/bin/sip
 
 # This module requires flex and bison to be built, try by loading
 # those that are installed...
 for m in flex bison ; do
     if [[ $(pack_installed $m) -eq 1 ]]; then
-	pack_cmd "module load $(pack_get --module-name-requirement $m) $(pack_get --module-name $m)"
+    pack_set -build-mod-req $m
     fi
 done
 
-p=$(pack_get --prefix)
-pack_cmd "$(get_parent_exec) configure.py -b $p/bin" \
-	 "-d $p/lib/python$pV/site-packages/ --stubsdir $p/lib/python$pV/site-packages/" \
-	 "-e $p/include -v $p/sip"
+#pack_cmd "$(get_parent_exec) lib/configure.py -b $p/bin" \
+#	 "-d $p/lib/python$pV/site-packages/ --stubsdir $p/lib/python$pV/site-packages/" \
+#	 "-e $p/include -v $p/sip"
 
-pack_cmd "make"
-pack_cmd "make install"
-
-# Unload again...
-for m in flex bison ; do
-    if [[ $(pack_installed $m) -eq 1 ]]; then
-	pack_cmd "module unload $(pack_get --module-name $m) $(pack_get --module-name-requirement $m)"
-    fi
-done
-
+pack_cmd "$_pip_cmd . --prefix=$(pack_get -prefix)"
 
