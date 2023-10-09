0baac2ab7fe4722b4793c41cc450f3476dbaec60
Nick Papior <nickpapior@gmail.com>
2023-10-09 09:58:41 +0200

    fixed arb and flint

--- libs/arb.bash
+++ libs/arb.bash
@@ -8,7 +8,16 @@ pack_set -lib -larb
 
 pack_set -mod-req flint
 
-pack_cmd "./configure --with-flint=$(pack_get -prefix flint)" \
+opts=
+if $(is_c gnu) ; then
+  _prefix=$(pack_get -prefix $(get_c -name)[$(get_c -version)])
+  opts="--with-gmp=$_prefix --with-mpfr=$_prefix)"
+else
+  pack_set -mod-req gmp -mod-req mpfr
+  opts="--with-gmp=$(pack_get -prefix gmp) --with-mpfr=$(pack_get -prefix mpfr)"
+fi
+
+pack_cmd "./configure --with-flint=$(pack_get -prefix flint) $opts" \
 	 "--prefix=$(pack_get -prefix)"
 pack_cmd "make"
 pack_cmd "make check > arb.test 2>&1"
--- libs/flint.bash
+++ libs/flint.bash
@@ -5,7 +5,15 @@ pack_set -s $MAKE_PARALLEL -s $IS_MODULE
 pack_set -install-query $(pack_get -LD)/libflint.a
 pack_set -lib -lflint
 
-pack_cmd "./configure --prefix=$(pack_get -prefix)"
+opts=
+if $(is_c gnu) ; then
+  _prefix=$(pack_get -prefix $(get_c -name)[$(get_c -version)])
+  opts="--with-gmp=$_prefix --with-mpfr=$_prefix)"
+else
+  pack_set -mod-req gmp -mod-req mpfr
+  opts="--with-gmp=$(pack_get -prefix gmp) --with-mpfr=$(pack_get -prefix mpfr)"
+
+pack_cmd "./configure --prefix=$(pack_get -prefix) $opts"
 pack_cmd "make $(get_make_parallel)"
 pack_cmd "make check > flint.test 2>&1 || echo 'forced'"
 pack_store flint.test
