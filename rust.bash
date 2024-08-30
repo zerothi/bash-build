8e240c88840286ae9827b0a538c9dc1bc05e6b6d
Nick Papior <nicpa@dtu.dk>
2024-08-30 13:25:26 +0200

    added rust to the packages
    Signed-off-by: Nick Papior <nicpa@dtu.dk>

---
+++ rust.bash
@@ -0,0 +1,17 @@
+
+for v in 1.80.1
+do
+  add_package -build generic -no-default-modules -alias rust -package rust \
+    https://static.rust-lang.org/dist/rust-$IRV-$(uname -m)-$(uname -i)-linux-gnu.tar.xz
+
+  pack_set -s $IS_MODULE
+
+  pack_set -install-query $(pack_get -prefix)/bin/rustc
+  pack_set -module-opt "-prepend-ENV PATH=~/.cargo/bin"
+
+  pack_cmd "./install.sh --prefix=$(pack_get -prefix) --verbose --disable-ldconfig"
+
+  # Needed as it is not source_pack
+  pack_install
+
+done
