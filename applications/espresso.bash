9dd47328adbae687e09760e94f08bcdb49a9855d
Nick Papior <nicpa@dtu.dk>
2024-12-06 10:14:47 +0100

    moved espresso to cmake
    Signed-off-by: Nick Papior <nicpa@dtu.dk>

--- applications/espresso.bash
+++ applications/espresso.bash
@@ -1,126 +1,69 @@
-for v in 6.8 7.2 7.3.1 7.4 ; do
-    tmp="-package q-espresso -version $v"
-    case $v in
-	6.2.1)
-	    tmp="$tmp https://gitlab.com/QEF/q-e/-/archive/qe-6.2.1/q-e-qe-6.2.1.tar.bz2"
-	    ;;
-	6.1.0)
-	    tmp="$tmp https://gitlab.com/QEF/q-e/-/archive/qe-6.1.0/q-e-qe-6.1.0.tar.bz2"
-	    ;;
-	5.4.0)
-	    tmp="$tmp http://www.qe-forge.org/gf/download/frsrelease/211/968/espresso-5.4.0.tar.gz"
-	    ;;
-	5.3.0)
-	    tmp="$tmp http://www.qe-forge.org/gf/download/frsrelease/204/912/espresso-5.3.0.tar.gz"
-	    ;;
-	5.2.1)
-	    tmp="$tmp http://www.qe-forge.org/gf/download/frsrelease/199/855/espresso-5.2.1.tar.gz"
-	    ;;
-	5.1.2)
-	    tmp="$tmp http://www.qe-forge.org/gf/download/frsrelease/185/753/espresso-5.1.2.tar.gz"
-	    ;;
-	5.1.1)
-	    tmp="$tmp http://www.qe-forge.org/gf/download/frsrelease/173/655/espresso-5.1.1.tar.gz"
-	    ;;
-	5.1)
-	    tmp="$tmp http://www.qe-forge.org/gf/download/frsrelease/151/581/espresso-5.1.tar.gz"
-	    ;;
-	5.0.3)
-	    tmp="$tmp http://qe-forge.org/gf/download/frsrelease/116/403/espresso-5.0.2.tar.gz"
-	    ;;
-	5.0.99)
-	    tmp="$tmp http://www.qe-forge.org/gf/download/frsrelease/151/519/espresso-5.0.99.tar.gz"
-	    ;;
-	*)
-	    tmp="$tmp https://gitlab.com/QEF/q-e/-/archive/qe-$v/q-e-qe-$v.tar.bz2"
-	    ;;
-    esac
+for v in 7.2 7.3.1 7.4 ; do
 
-    add_package $tmp
-    
-    pack_set -install-query $(pack_get -prefix)/bin/pw.x
+add_package -package q-espresso -version $v \
+  https://gitlab.com/QEF/q-e/-/archive/qe-$v/q-e-qe-$v.tar.bz2
 
-    pack_set -module-opt "-lua-family q-espresso"
+pack_set -s $BUILD_DIR -s $MAKE_PARALLEL
 
-    pack_set -module-requirement mpi 
-    pack_set -module-requirement fftw
+pack_set -install-query $(pack_get -prefix)/bin/pw.x
 
-    # Fetch all the packages and pack them out
-    source applications/espresso-packages.bash
+pack_set -module-opt "-lua-family q-espresso"
 
-    if [ -z "$FLAG_OMP" ]; then
-	doerr q-espresso "Can not find the OpenMP flag (set FLAG_OMP in source)"
-    fi
-    tmp=
+pack_set $(list -prefix '-mod-req ' mpi hdf5 fftw libxc wannier90 elpa)
 
-    # Check for Intel MKL or not
-    tmp_lib="FFT_LIBS='$(list -LD-rp-lib[omp] fftw)'"
-    # BLACS is always empty (fully encompassed in scalapack)
-    tmp_lib="$tmp_lib BLACS_LIBS="
-    if $(is_c intel) ; then
-        tmp="$tmp -L$MKL_PATH/lib/intel64 $RPATH_LINE$MKL_PATH/lib/intel64"
-	tmp=${tmp//\/\//}
-	tmp_lib="$tmp_lib BLAS_LIBS='$tmp -lmkl_blas95_lp64 -qmkl=parallel'"
-	# Newer versions does not rely on separation of BLACS and ScaLAPACK
-    	tmp_lib="$tmp_lib SCALAPACK_LIBS='$tmp -lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64'"
-        tmp_lib="$tmp_lib LAPACK_LIBS='$tmp -lmkl_lapack95_lp64'"
+if [ -z "$FLAG_OMP" ]; then
+  doerr q-espresso "Can not find the OpenMP flag (set FLAG_OMP in source)"
+fi
 
-    else
-	pack_set -module-requirement scalapack
+# Create the CMAKE flags
+opts=
 
-	la=lapack-$(pack_choice -i linalg)
-	pack_set -module-requirement $la
-	tmp_ld="$(list -LD-rp +$la)"
-	tmp_lib="$tmp_lib LAPACK_LIBS='$tmp_ld $(pack_get -lib[omp] $la)'"
-	tmp_lib="$tmp_lib SCALAPACK_LIBS='$(list -LD-rp scalapack) -lscalapack'"
-	tmp_lib="$tmp_lib BLAS_LIBS='$tmp_ld $(pack_get -lib[omp] $la)'"
+# Add default stuff
+opts="$opts --log-level=debug"
+opts="$opts -DCMAKE_INSTALL_PREFIX=$(pack_get -prefix)"
+opts="$opts -DCMAKE_BUILD_TYPE=Release"
+opts="$opts -DCMAKE_C_COMPILER='$MPICC'"
+opts="$opts -DCMAKE_C_FLAGS='$CFLAGS'"
+opts="$opts -DCMAKE_Fortran_COMPILER='$MPIFC'"
+opts="$opts -DCMAKE_Fortran_FLAGS='$FFLAGS'"
 
-    fi
+# Environ is something that enables continuum embedding calculations
+opts="$opts -DQE_ENABLE_ENVIRON=off"
+opts="$opts -DQE_ENABLE_MPI=on"
+opts="$opts -DQE_ENABLE_MPI_INPLACE=on"
+opts="$opts -DQE_ENABLE_OPENMP=on"
+opts="$opts -DQE_ENABLE_SCALAPACK=on"
+opts="$opts -DQE_ENABLE_ELPA=on"
+opts="$opts -DELPA_Fortran_MODS_DIR=$(pack_get -I elpa)/elpa"
+opts="$opts -DQE_ENABLE_LIBXC=on"
+opts="$opts -DLIBXC_ROOT=$(pack_get -prefix libxc)"
+opts="$opts -DQE_ENABLE_HDF5=on"
+opts="$opts -DHDF5_ROOT=$(pack_get -prefix hdf5)"
+opts="$opts -DQE_ENABLE_FOX=on"
 
-    # If we are in 6.0 we should fix CPV/src/forces.f90
-    if [[ $(vrs_cmp $v 6.0.0) -le 0 ]]; then
-	pack_cmd "sed -i -e '99,102s: \&:, \&:g' CPV/src/forces.f90"
-    else
-	pack_set -module-requirement libxc
-	pack_set -module-requirement hdf5
-	tmp_lib="$tmp_lib --with-libxc=yes --with-libxc-prefix=$(pack_get -prefix libxc)"
-	tmp_lib="$tmp_lib --with-hdf5=yes --with-hdf5-libs='$(list -LD-rp hdf5) $(pack_get -lib[fortran] hdf5)'"
-    fi
+opts="$opts -DQE_FFTW3_VENDOR=FFTW3"
+opts="$opts -DFFTW3_ROOT=$(pack_get -prefix fftw)"
 
-    # Install commands that it should run
-    tmp="$tmp -D__FFTW3 ${FFLAGS//-floop-block/}"
-    tmp="${tmp//-qopt-prefetch/}"
-    tmp="${tmp//-opt-prefetch/}"
-    tmp_inc="$(list -INCDIRS $(pack_get -mod-req-path))"
-    pack_cmd "./configure" \
-	 "$tmp_lib" \
-	 "FFLAGS='$tmp $tmp_inc $FLAG_OMP'" \
-	 "FFLAGS_NOOPT='-fPIC'" \
-	 "FCFLAGS='$tmp $tmp_inc $FLAG_OMP'" \
-	 "FCFLAGS_NOOPT='-fPIC'" \
-	 "LDFLAGS='$(list -LD-rp $(pack_get -mod-req-path)) $FLAG_OMP'" \
-	 "CPPFLAGS='$(list -INCDIRS $(pack_get -mod-req-path))'" \
-	 "--enable-parallel --enable-openmp" \
-	 "--prefix=$(pack_get -prefix)"
+opts="$opts -DQE_ENABLE_WANNIER90=on"
+opts="$opts -DWANNIER90_ROOT=$(pack_get -prefix wannier90)"
 
-    # Make commands
-    for EXE in $libs ; do
- 	pack_cmd "make $(get_make_parallel) $EXE"
-    done
+lapack_opts=
+if $(is_c intel) ; then
+  qe_la=mkl
+  opts="$opts -DSCALAPACK_LIBRARY=-mkl=cluster"
+  lapack_opts="-DLAPACK_LIBRARY=-mkl=cluster"
 
-    # Prepare installation directories...
-    pack_cmd "mkdir -p $(pack_get -prefix)/bin"
-    pack_cmd "mkdir -p $(pack_get -LD)"
-    pack_cmd "mkdir -p $(pack_get -prefix)/include"
-    if [[ $(vrs_cmp $v 6.0.0) -ge 0 ]]; then
-	pack_cmd "make links"
-	pack_cmd "make install"
-    fi
-    pack_cmd "cp bin/* $(pack_get -prefix)/bin/"
-    pack_cmd "cp make.inc $(pack_get -prefix)/"
-    # Install the iotk-library (not handled in install)
-    pack_cmd "[ -d iotk ] && cp iotk/src/libiotk.a $(pack_get -LD)/ || true"
-    pack_cmd "[ -d iotk ] && cp iotk/src/*.mod $(pack_get -prefix)/include/ || true"
+elif $(is_c gnu) ; then
+  pack_set -module-requirement scalapack
+  qe_la=$(pack_choice -i linalg)
+  la=lapack-$qe_la
+  pack_set -module-requirement $la
+  opts="$opts -DSCALAPACK_LIBRARY='$(pack_get -lib scalapack)'"
+  lapack_opts="-DLAPACK_LIBRARY='$(pack_get -lib[omp] $la)'"
+fi
+
+pack_cmd "cmake -B. -S.. $opts $lapack_opts"
+pack_cmd "cmake --build . $(get_make_parallel) --target install"
 
 done
 
