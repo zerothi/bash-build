0df502e068d7cdfaa8d99ab00f3ee599bfa931f5
Nick Papior <nicpa@dtu.dk>
2025-03-20 08:52:32 +0100

    added petsc4py and slepc4py in complex variants
    Signed-off-by: Nick Papior <nicpa@dtu.dk>

--- python/petsc4py.bash
+++ python/petsc4py.bash
@@ -1,14 +1,16 @@
-add_package -package petsc4py \
-	    -directory $(pack_get -directory petsc-d) \
-	    $(pack_get -archive petsc-d)
+for prec in d z ; do
+
+add_package -package petsc4py-$prec \
+  -directory $(pack_get -directory petsc-$prec) \
+  $(pack_get -archive petsc-$prec)
 
 pack_set -s $IS_MODULE
 
 pack_set -build-mod-req cython
-pack_set -module-requirement petsc-d \
-	 -module-requirement mpi4py -module-requirement numpy
+pack_set -module-requirement petsc-$prec \
+  -module-requirement mpi4py -module-requirement numpy
 
-pack_set -install-query $(pack_get -LD)/python$pV/site-packages/$(pack_get -alias)/__init__.py
+pack_set -install-query $(pack_get -LD)/python$pV/site-packages/petsc4py/__init__.py
 
 pack_cmd "cd src/binding/petsc4py"
 pack_cmd "$_pip_cmd . --prefix=$(pack_get -prefix)"
@@ -17,4 +19,4 @@ add_test_package petsc4py.test
 pack_cmd "nosetests --exe petsc4py > $TEST_OUT 2>&1 || echo forced"
 pack_store $TEST_OUT
 
-
+done
--- python/slepc4py.bash
+++ python/slepc4py.bash
@@ -1,15 +1,19 @@
+for prec in d z ; do
 v=3.18.3
-add_package https://files.pythonhosted.org/packages/source/s/slepc4py/slepc4py-$v.tar.gz
+add_package -package slepc4py-$prec \
+  https://files.pythonhosted.org/packages/source/s/slepc4py/slepc4py-$v.tar.gz
 
 pack_set -s $IS_MODULE
 
-pack_set --module-requirement petsc4py \
-    --module-requirement slepc-d
+pack_set --module-requirement petsc4py-$prec \
+  --module-requirement slepc-$prec
 
-pack_set --install-query $(pack_get --LD)/python$pV/site-packages/$(pack_get --alias)/__init__.py
+pack_set -install-query $(pack_get -LD)/python$pV/site-packages/slepc4py/__init__.py
 
 pack_cmd "$_pip_cmd . --prefix=$(pack_get -prefix)"
 
 add_test_package slepc4py.test
 pack_cmd "nosetests --exe slepc4py > $TEST_OUT 2>&1 || echo forced"
 pack_store $TEST_OUT
+
+done
