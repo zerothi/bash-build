16299eff01e23b26470c76625518dc69dc85d94b
Nick Papior <nicpa@dtu.dk>
2026-09-17 08:54:46 +0200

    updated nodejs
    Signed-off-by: Nick Papior <nicpa@dtu.dk>

--- helpers/nodejs.bash
+++ helpers/nodejs.bash
@@ -1,10 +1,12 @@
-v=18.15.0
-add_package -package nodejs -version $v -build generic \
-       https://nodejs.org/dist/v$v/node-v$v-linux-x64.tar.xz
+for v in 22.23.2 24.21.0 26.8.2
+do
+  add_package -package nodejs -version $v -build generic \
+         https://nodejs.org/dist/v$v/node-v$v-linux-x64.tar.xz
 
-pack_set -s $IS_MODULE
+  pack_set -s $IS_MODULE
 
-pack_set -install-query $(pack_get -prefix)/bin/node
+  pack_set -install-query $(pack_get -prefix)/bin/node
 
-pack_cmd "mkdir -p $(pack_get -prefix)"
-pack_cmd "mv bin CHANGELOG.md include lib LICENSE README.md share $(pack_get -prefix)/"
+  pack_cmd "mkdir -p $(pack_get -prefix)"
+  pack_cmd "mv bin CHANGELOG.md include lib LICENSE README.md share $(pack_get -prefix)/"
+done
